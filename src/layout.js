72a7d0b09b348ad3e4f40a70196915d656153862
Bartek Pampuch <bartosz.pampuch@comarch.pl>
2014-01-22 00:02:52 +0100

    TextTools turn texts into Lines

--- src/layout.js
+++ src/layout.js
@@ -1,4 +1,7 @@
 (function() {
+	////////////////////////////////////////
+	// Line
+
 	/**
 	 * Creates an instance of Line
 	 *
@@ -106,8 +109,190 @@
 	};
 
 
+
+
+	////////////////////////////////////////
+	// TextTools
+
+	var TextTools = (function(){
+		var WORD_RE = /([^ ,\/!.?:;\-\n]*[ ,\/!.?:;\-]*)|\n/g;
+		var LEADING = /^(\s)+/g;
+		var TRAILING = /(\s)+$/g;
+
+		/**
+		 * Creates an instance of TextTools - a helper which turns text into a set of Lines
+		 * @param {FontProvider} fontProvider
+		 */
+		function TextTools(fontProvider) {
+			this.fontProvider = fontProvider;
+		}
+
+		/**
+		 * Converts an array of strings (or inline-definition-objects) into a set of Lines
+		 * @param  {Object} textArray - an array of inline-definition-objects (or strings)
+		 * @param  {Number} maxWidth - max width a single Line should have
+		 * @return {Array} an array of Lines
+		 */
+		TextTools.prototype.buildLines = function(textArray, maxWidth) {
+			var measured = measure(this.fontProvider, textArray/*, defaultStyle*/);
+
+			var lines = [];
+
+			var currentLine = new Line(maxWidth);
+			lines.push(currentLine);
+
+			var nextInline = measured.shift();
+
+			while (nextInline) {
+				if (currentLine.addInline(nextInline)) {
+					nextInline = measured.shift();
+				} else {
+					currentLine = new Line(maxWidth);
+					lines.push(currentLine);
+				}
+			}
+
+			return lines;
+		}
+
+		function splitWords(text) {
+			var results = [];
+
+			var array = text.match(WORD_RE);
+
+			// i < l - 1, because the last match is always an empty string
+			// other empty strings however are treated as new-lines
+			for(var i = 0, l = array.length; i < l - 1; i++) {
+				var item = array[i];
+
+				var isNewLine = item.length === 0;
+
+				if (!isNewLine) {
+					results.push({text: item});
+				} 
+				else {
+					var shouldAddLine = (results.length === 0 || results[results.length - 1].lineEnd);
+
+					if (shouldAddLine) {
+						results.push({ text: '', lineEnd: true });
+					}
+					else {
+						results[results.length - 1].lineEnd = true;
+					}
+				}
+			}
+
+			return results;
+		}
+
+		function copyStyle(source, destination) {
+			destination = destination || {};
+			source = source || {}; //TODO: default style
+
+			[ 'bold', 'italics', 'fontSize', 'font' ].forEach(function(key){
+				if (source[key]) {
+					destination[key] = source[key];
+				}
+			});
+
+			return destination;
+		}
+
+		function normalizeTextArray(array) {
+			var results = [];
+
+			for(var i = 0, l = array.length; i < l; i++) {
+				var item = array[i];
+				var style = null;
+				var words;
+
+				if (typeof item == 'string' || item instanceof String) {
+					words = splitWords(item);
+				} else {
+					words = splitWords(item.text);
+					style = copyStyle(item);
+				}
+
+				for(var i2 = 0, l2 = words.length; i2 < l2; i2++) {
+					var result = { 
+						text: words[i2].text 
+					};
+
+					if (words[i2].lineEnd) { 
+						result.lineEnd = true; 
+					}
+
+					copyStyle(style, result)
+
+					results.push(result);
+				}
+			}
+
+			return results;
+		}
+
+		//TODO: support for other languages (currently only polish is supported)
+		var diacriticsMap = { 'Ą': 'A', 'Ć': 'C', 'Ę': 'E', 'Ł': 'L', 'Ń': 'N', 'Ó': 'O', 'Ś': 'S', 'Ź': 'Z', 'Ż': 'Z', 'ą': 'a', 'ć': 'c', 'ę': 'e', 'ł': 'l', 'ń': 'n', 'ó': 'o', 'ś': 's', 'ź': 'z', 'ż': 'z'};
+
+		function removeDiacritics(text) {
+			return text.replace(/[^A-Za-z0-9\[\] ]/g, function(a) {
+				return diacriticsMap[a] || a;
+			});
+		}
+
+		function measure(fontProvider, textArray, defaultStyle) {
+			var normalized = normalizeTextArray(textArray);
+
+			normalized.forEach(function(item) {
+				var fontName = item.font || defaultStyle && defaultStyle.font || 'Roboto';
+				var fontSize = item.fontSize || defaultStyle && defaultStyle.fontSize || 12;
+				//var color = item.color || defaultStyle && defaultStyle.color || "black";
+
+				var font = fontProvider.provideFont(fontName, item.bold, item.italics);
+
+				// TODO: character spacing
+				item.width = font.widthOfString(removeDiacritics(item.text), fontSize);
+				item.height = font.lineHeight(fontSize);
+
+				var leadingSpaces = item.text.match(LEADING);
+				var trailingSpaces = item.text.match(TRAILING);
+				if (leadingSpaces) {
+					item.leadingCut = font.widthOfString(leadingSpaces[0], fontSize);
+				}
+				else { 
+					item.leadingCut = 0;
+				}
+
+				if (trailingSpaces) {
+					item.trailingCut = font.widthOfString(trailingSpaces[0], fontSize);
+				}
+				else {
+					item.trailingCut = 0;
+				}
+
+				item.font = font;
+			})
+
+			return normalized;
+		}
+
+		//****TESTS**** (remove first '/' to comment)
+		TextTools.prototype.splitWords = splitWords;
+		TextTools.prototype.normalizeTextArray = normalizeTextArray;
+		TextTools.prototype.measure = measure;
+		// */
+
+		return TextTools;
+	})();
+	
+	
+
+	////////////////////////////////////////
+	// Exports
+
 	var pdfMake = {
 		Line: Line,
+		TextTools: TextTools,
 	};
 
 	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 
 var pdfMake = require('../src/layout.js');
 var Line = pdfMake.Line;
+var TextTools = pdfMake.TextTools;
 
 describe('Line', function() {
 	describe('addInline', function() {
@@ -145,4 +146,185 @@ describe('Line', function() {
 		it('should take into account baseline offsets', function() {
 		});
 	})
+});
+
+
+
+describe('TextTools', function() {
+	var sampleText = 'Przyklad, bez nowych linii,   ale !!!! rozne!!!konstrukcje i ..blablablabla.';
+	var sampleText2 = 'Przyklad, z nowy\nmi liniami\n, \n \n  ale\n\n !!!! rozne!!!konstrukcje i ..blablablabla.';
+
+	var plainTextArray = [
+		'Imię: ',
+		'Jan   ',
+		'   Nazwisko:',
+		' Nowak\nDodatkowe informacje:'
+	]
+
+	var mixedTextArray = [
+		{ text: 'Imię: ', bold: true },
+		'Jan   ',
+		{ text: '   Nazwisko:', bold: true },
+		{ text: ' Nowak\nDodatkowe informacje:', bold: true }
+	]
+
+	var plainTextArrayWithoutNewLines = [
+		'Imię: ',
+		'Jan   ',
+		'   Nazwisko:',
+		' Nowak Dodatkowe informacje:'
+	]
+
+	var sampleTestProvider = {
+		provideFont: function(familyName, bold, italics) {
+			return {
+				widthOfString: function(text, size) {
+					return text.length * size * (bold ? 1.5 : 1);
+				},
+				lineHeight: function(size) {
+					return size;
+				}
+			}
+		}
+	};
+
+	var textTools = new TextTools(sampleTestProvider);
+
+	describe('splitWords', function() {
+		it('should do basic splitting', function() {
+			var result = textTools.splitWords(sampleText);
+			assert.equal(result.length, 9);
+		});
+
+		it('should not set lineEnd on inlines if there are no new-lines', function() {
+			var result = textTools.splitWords(sampleText);
+
+			result.forEach(function(item) {
+				assert.notEqual(item.lineEnd, true);
+			})
+		});
+
+		it('should split into lines if there are new-line chars', function() {
+			var result = textTools.splitWords(sampleText2);
+			assert.equal(result.length, 15);
+		});
+
+		it('should split properly when adjacent newlines appear', function() {
+			var result = textTools.splitWords(sampleText2);
+			assert.equal(result[9].text.length, 0);			
+			assert.equal(result[9].lineEnd, true);
+		});
+
+		it('should support whitespace-only lines', function() {
+			var result = textTools.splitWords(sampleText2);
+			assert.equal(result[6].text, ' ');			
+			assert.equal(result[6].lineEnd, true);
+		})
+	});
+
+	describe('normalizeTextArray', function() {
+		it('should support plain strings', function() {
+			var result = textTools.normalizeTextArray(plainTextArray);
+			assert.equal(result.length, 8);
+		});
+
+		it('should support new lines in plain strings', function() {
+			var result = textTools.normalizeTextArray(plainTextArray);
+			assert.equal(result[5].lineEnd, true);
+		});
+
+		it('should support arrays with style definition', function() {
+			var result = textTools.normalizeTextArray(mixedTextArray);
+			assert.equal(result.length, 8);
+		});
+
+		it('should keep style definition after splitting', function() {
+			var result = textTools.normalizeTextArray(mixedTextArray);
+			[0, 2, 3, 4, 5, 6, 7].forEach(function(i) { 
+				assert.equal(result[i].bold, true);
+			});
+
+			assert(!result[1].bold);
+		});
+	});
+
+	describe('measure', function() {
+		it('should use default style', function(){
+			var result = textTools.measure(sampleTestProvider, plainTextArray);
+			assert.notEqual(result, null);
+			assert.notEqual(result.length, 0);
+		});
+
+		it('should set width', function() {
+			var result = textTools.measure(sampleTestProvider, plainTextArray);
+			assert.notEqual(result, null);
+			assert.notEqual(result.length, 0);
+			assert.notEqual(result[0].width, null);
+		});
+
+		it('should measure text widths', function() {
+			var result = textTools.measure(sampleTestProvider, plainTextArray);
+			assert.equal(result[0].width, 72);
+			assert.equal(result[2].width, 36);
+			assert.equal(result[3].width, 108);
+		});
+
+		it('should calculate leading and trailing cuts', function() {
+			var result = textTools.measure(sampleTestProvider, plainTextArray);
+			assert.equal(result[0].trailingCut, 12);
+			assert.equal(result[0].leadingCut, 0);
+		});
+
+		it('should set the same value for leading and trailing cuts for whitespace-only strings', function() {
+			var result = textTools.measure(sampleTestProvider, plainTextArray);
+			assert.equal(result[2].trailingCut, 36);
+			assert.equal(result[2].leadingCut, 36);
+		});
+
+		it('should set leading and trailing cuts to 0 if texts cannot be trimmed', function() {
+			var result = textTools.measure(sampleTestProvider, plainTextArray);
+			assert.equal(result[5].trailingCut, 0);
+			assert.equal(result[5].leadingCut, 0);
+		});
+
+		it('should support default style overrides', function() {
+			var result = textTools.measure(sampleTestProvider, plainTextArray, { fontSize: 100 });
+			assert.equal(result[0].width, 600);
+		});
+
+		it('should take into account styles defined in textArrays', function() {
+			var result = textTools.measure(sampleTestProvider, mixedTextArray);
+			assert.equal(result[0].width, 108);
+			assert.equal(result[0].trailingCut, 18);
+			assert.equal(result[0].leadingCut, 0);
+			assert.equal(result[0].bold, true);
+
+			assert.equal(result[1].width, 72);
+			assert.equal(result[1].trailingCut, 36);
+			assert.equal(result[1].leadingCut, 0);
+			assert(!result[1].bold);
+		});
+	});
+
+	describe('buildLines', function() {
+		it('should create one line if there is enough space and text contains no newlines', function(){
+			var lines = textTools.buildLines(plainTextArrayWithoutNewLines, 1000);
+			assert.equal(lines.length, 1);
+		});
+
+		it('should split text into lines if there is not enough space', function(){
+			var lines = textTools.buildLines(plainTextArrayWithoutNewLines, 100);
+			assert(lines.length > 1);
+		});
+
+		it('should split text into lines even if there is enough space but text contains new lines', function(){
+			var lines = textTools.buildLines(plainTextArray, 1000);
+			assert.equal(lines.length, 2);
+		});
+
+		it('should take into account styled inlines in text', function() {
+			var lines = textTools.buildLines(mixedTextArray, 72+36);
+			assert.equal(lines.length, 6);
+		});
+	});
 });
