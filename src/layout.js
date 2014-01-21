9e0dd078167a78d680b97238d306e64cea4dc360
Bartek Pampuch <bartosz.pampuch@comarch.pl>
2014-01-21 23:01:13 +0100

    Line implementation

--- src/layout.js
+++ src/layout.js
@@ -4,27 +4,77 @@
 	 *
 	 * @constructor
 	 * @this {Line}
-	 * @param {[type]} Maximum width this line can have
+	 * @param {Number} Maximum width this line can have
 	 */
 	function Line(maxWidth) {
+		this.clear(maxWidth);
 	}
+
+	/**
+	 * Removes all inlines from the Line and sets new maxWidth
+	 * @param  {Number} new maximum width this line can have
+	 */
+	Line.prototype.clear = function(newMaxWidth) {
+		this.maxWidth = newMaxWidth || this.maxWidth;
+		this.inlineWidths = 0;
+		this.leadingCut = 0;
+		this.trailingCut = 0;
+		this.newLineForced = false;
+		this.inlines = [];
+	};
 
 	/**
 	 * Adds an inline to the Line if there's enough space left
 	 * @param {Object} inline 
-	 * @return {Boolean}
+	 * @return {Boolean} boolean value indicating whether inline has been added (there was enough space)
 	 */
 	Line.prototype.addInline = function(inline) {
+		if (this.newLineForced) return false;
+
+		if (inline.leadingCut === inline.trailingCut && inline.leadingCut === inline.width) {
+			// double-trimming fix
+			inline.leadingCut = 0;
+			inline.trailingCut = 0;
+			inline.width = 0;
+		}
+
+		var leadingCut;
+
+		if (this.inlines.length === 0) {
+			leadingCut = inline.leadingCut || 0;
+		}
+		else {
+			leadingCut = this.leadingCut;
+		}
+
+		var trailingCut = inline.trailingCut || 0;
+
+		if (this.inlineWidths + inline.width - leadingCut - trailingCut <= this.maxWidth || this.inlines.length === 0) {
+			this.leadingCut = leadingCut;
+			this.trailingCut = trailingCut;
+
+			inline.x = this.inlineWidths - this.leadingCut;
+
+			this.inlines.push(inline);
+			this.inlineWidths += inline.width;
+
+			if (inline.lineEnd) {
+				this.newLineForced = true;
+			}
+
+			return true;
+		}
+
 		return false;
-	}
+	};
 
 	/**
 	 * Returns line width for the specified maxWidth
 	 * @return {Number} width of the Line
 	 */
 	Line.prototype.getWidth = function() {
-		return 0;
-	}
+		return this.inlineWidths - this.leadingCut - this.trailingCut;
+	};
 
 	/**
 	 * Returns width of the widest inline (minimum maxWidth the Line could 
@@ -32,22 +82,33 @@
 	 * @return {Number} minimum width
 	 */
 	Line.prototype.getMinWidth = function() {
-		return 0;
-	}
+		var max = 0;
+		for(var i = 0, l = this.inlines.length; i < l; i++) {
+			var item = this.inlines[i];
+			max = Math.max(max, (item.width || 0) - (item.leadingCut || 0) - (item.trailingCut || 0));
+		}
+
+		return max;
+	};
 
 	/**
 	 * Returns line height
 	 * @return {Number}
 	 */
 	Line.prototype.getHeight = function() {
-		return 0;
-	}
+		var max = 0;
+		for(var i = 0, l = this.inlines.length; i < l; i++) {
+			var item = this.inlines[i];
+			max = Math.max(max, item.height || 0);
+		}
 
+		return max;
+	};
 
 
 	var pdfMake = {
 		Line: Line,
-	}
+	};
 
 	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
 		module.exports = pdfMake;
--- tests/test.js
+++ tests/test.js
@@ -5,20 +5,144 @@ var Line = pdfMake.Line;
 
 describe('Line', function() {
 	describe('addInline', function() {
-		it('should add inline if there is enough space');
-		it('should add first inline even if theres not enough space');
-		it('should not add following inlines if theres not enough space');
-		it('should trim-left first inline');
-		it('should not trim-left next inlines');
-		it('should trim-right last inline');
-		it('should not trim-right previous inlines');
-		it('should set x property for all inlines')
+		it('should add inline if there is enough space', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 50 }));
+			assert.equal(line.inlines.length, 1);
+		});
+		it('should add first inline even if theres not enough space', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 170 }));
+			assert.equal(line.inlines.length, 1);
+		});
+		it('should not add following inlines if theres not enough space', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 70 }));
+			assert(!line.addInline({ width: 40 }));
+			assert.equal(line.inlines.length, 1);
+		});
+		it('should take into account first inline leadingCut (left-trimming) when deciding if theres enough space', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 70, leadingCut: 20 }));
+			assert(line.addInline({ width: 40 }));
+			assert.equal(line.inlines.length, 2);
+		});
+		it('should not take into account following inline leadingCuts (left-trimming) when deciding if theres enough space', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 70, leadingCut: 20 }));
+			assert(line.addInline({ width: 20, leadingCut: 10 }));
+			assert(line.addInline({ width: 20, leadingCut: 10 }));
+			assert(!line.addInline({ width: 20, leadingCut: 10 }));
+			assert.equal(line.inlines.length, 3);
+		});
+		it('should take into account last inline trailingCut (right-trimming) when deciding if theres enough space', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 30 }));
+			assert(line.addInline({ width: 40 }));
+			assert(line.addInline({ width: 50, trailingCut: 20 }));
+			assert.equal(line.inlines.length, 3);
+		});
+		it('should not take into account previous inline trailingCuts (right-trimming) when deciding if theres enough space', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 40, trailingCut: 20 }));
+			assert(line.addInline({ width: 30, trailingCut: 20 }));
+			assert(!line.addInline({ width: 31 }));
+			assert.equal(line.inlines.length, 2);
+		});
+
+		it('should set x to 0 for first inline if there is no left-trimming (leadingCut)', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 40, leadingCut: 0, trailingCut: 20 }));
+			assert.equal(line.inlines[0].x, 0);
+		});
+
+		it('should set x to sum of preceding inline widths if there is no left-trimming', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 40, leadingCut: 0, trailingCut: 0 }));
+			assert(line.addInline({ width: 40, leadingCut: 0, trailingCut: 0 }));
+			assert(line.addInline({ width: 10, leadingCut: 0, trailingCut: 0 }));
+			assert.equal(line.inlines[2].x, 80);
+		});
+
+		it('should set x to -leadingCut for first inline when its left-trimmed', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 40, leadingCut: 10, trailingCut: 20 }));
+			assert.equal(line.inlines[0].x, -10);
+		});
+
+		it('should set x to 0 for second inline if first inline is fully trimmed (cut)', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 40, leadingCut: 40, trailingCut: 40 }));
+			assert(line.addInline({ width: 40, leadingCut: 40, trailingCut: 40 }));
+			assert.equal(line.inlines[1].x, 0);
+		});
+
+		it('should set x to sum of preceding inline widths minus first inline leadingCut', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 40, leadingCut: 30, trailingCut: 0 }));
+			assert(line.addInline({ width: 40, leadingCut: 0, trailingCut: 0 }));
+			assert(line.addInline({ width: 10, leadingCut: 0, trailingCut: 0 }));
+			assert.equal(line.inlines[2].x, 80 - 30);
+		});
+
+		it('should not subtract leadingCuts other than the from first inline when setting x', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 40, leadingCut: 30, trailingCut: 0 }));
+			assert(line.addInline({ width: 40, leadingCut: 10, trailingCut: 0 }));
+			assert(line.addInline({ width: 10, leadingCut: 5, trailingCut: 0 }));
+			assert.equal(line.inlines[2].x, 80 - 30);
+		});
+
+		it('should ignore trailingCuts when setting x', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 40, leadingCut: 30, trailingCut: 10 }));
+			assert(line.addInline({ width: 40, leadingCut: 10, trailingCut: 10 }));
+			assert(line.addInline({ width: 20, leadingCut: 5, trailingCut: 5 }));
+			assert.equal(line.inlines[2].x, 80 - 30);
+		});
 	});
 
 	describe('getWidth', function() {
+		it('should return sum of all inline widths', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 30 }));
+			assert(line.addInline({ width: 20 }));
+			assert(line.addInline({ width: 5 }));
+			assert.equal(line.getWidth(), 55);
+		});
+
+		it('should subtract first inline leadingCut (left-trimming) from total width', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 30, leadingCut: 20 }));
+			assert(line.addInline({ width: 20, leadingCut: 10 }));
+			assert(line.addInline({ width: 5, leadingCut: 3 }));
+			assert.equal(line.getWidth(), 55 - 20);
+		});
+
+		it('should subtract last inline trailingCut (right-trimming) from total width', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 30, leadingCut: 20, trailingCut: 5 }));
+			assert(line.addInline({ width: 20, leadingCut: 10, trailingCut: 3 }));
+			assert(line.addInline({ width: 5, leadingCut: 3, trailingCut: 1 }));
+			assert.equal(line.getWidth(), 55 - 20 - 1);
+		});
 	});
 
 	describe('getMinWidth', function() {
-		it('should return longest inline trimmed width');
+		it('should return longest inline trimmed width', function() {
+			var line = new Line(100);
+			assert(line.addInline({ width: 30, leadingCut: 10, trailingCut: 5 }));
+			assert(line.addInline({ width: 20, leadingCut: 3, trailingCut: 7 }));
+			assert(line.addInline({ width: 40, leadingCut: 9, trailingCut: 6 }));
+			assert.equal(line.getMinWidth(), 25);
+		});
+	});
+
+	describe.skip('getHeight', function() {
+		it('should return highest inline height when baselines are equal', function() {
+		});
+
+		it('should take into account baseline offsets', function() {
+		});
 	})
-})
+});
