63f32e0bac71d224fd53cf06e44d7feaf1b31e8d
Bartek Pampuch <bartosz.pampuch@comarch.pl>
2014-01-21 17:13:52 +0100

    Initial structure

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  // Add the grunt-mocha-test tasks.
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.initConfig({
+    // Configure a mochaTest task
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['tests/**/*.js']
+      }
+    }
+  });
+
+  grunt.registerTask('default', 'mochaTest');
+
+};
---
+++ tests/test.js
@@ -0,0 +1,6 @@
+var assert = require('assert');
+
+
+describe('Mocha', function() {
+	it ('should work', function() { });
+});
