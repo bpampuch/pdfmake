b3ee95d932043d7d1864c1482f9f77e15e6e1cf5
Bartek Pampuch <bartosz.pampuch@gmail.com>
2014-03-04 02:06:31 +0100

    Sample table definition (for testing purpose)
    Eventually another the sample definition for the table will be provided
    
    table example update

--- examples/tables.js
+++ examples/tables.js
@@ -11,9 +11,30 @@ var PdfPrinter = require('../src/printer');
 var printer = new PdfPrinter(fonts);
 
 
+var desc = [{
+	table: {
+		widths: 'auto',
+		body: []
+	}
+}];
+
+for(var i = 0; i < 47; i++) {
+	desc[0].table.body.push(['a', 'b', 'c']);
+}
+
+//desc[0].table.body.push(['a\nb\nc', 'a\nb\nc', 'a\nb\nc']);
+
+
 var docDefinition = {
+
 	content: [
-		{ text: 'Tables', style: 'header' },
+		// desc[0],
+		{ stack: [
+				'Tables',
+				{ text: 'This file will eventually be replaced. It\'s here for testing purpose', fontSize: 11, bold: false }
+			],
+			style: 'header'
+		},
 		{
 			table: {
 				// cellBorder: 1,
@@ -22,11 +43,12 @@ var docDefinition = {
 				//evenRowCellBorder
 				// tableBorder: { lineWidth: 1, dash: { length: 3, space: 2 } },
 
-				headerLines: 1,
+				headerRows: 1,
+				// keepWithHeaderRows: 1,
 				widths: [ '*', '*', 'auto', 'auto' ],
-				body: 
+				body:
 				[
-					[ 'Header 1', 'H2', 'Header\nwith\nlines', { text: 'last', style: 'bigger' } ],
+					[ 'Header 1', 'H2', 'Header\nwith\nlines', { text: 'bigger header', style: 'bigger' } ],
 					[ 'Column 1', 'Column 2', 'Column 3', 'Column 4' ],
 					[ 'A text in the first column', 'Text in the second one', 'Other things go here', 'or here' ],
 					[ { ol: [
@@ -114,7 +136,7 @@ var docDefinition = {
 		header: {
 			fontSize: 18,
 			bold: true,
-			margin: [0, 0, 0, 20]
+			margin: [0, 0, 0, 40]
 		},
 		bigger: {
 			fontSize: 15,
