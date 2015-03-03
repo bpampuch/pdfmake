8836cd110c2fae549296c2036edd418e4791e303
Gregor Robert Russbuelt <grussbuelt@thoughtworks.com>
2015-03-03 11:13:56 +0100

    image wrapping: add integration test for lists

--- tests/integrationTest.js
+++ tests/integrationTest.js
@@ -13,6 +13,8 @@ describe('Integration Test', function () {
   var MARGINS = {top: 40, left: 40, right: 40, bottom: 40};
   var LINE_HEIGHT = 14.064;
 
+	var DEFAULT_BULLET_SPACER = '9. ';
+
   function renderPages(sizeName, docDefinition) {
     var size = sizes[sizeName];
     docDefinition.images = docDefinition.images || {};
@@ -408,7 +410,7 @@ describe('Integration Test', function () {
 			assert.equal(pages.length, 1);
 			assert.equal(lines.length, 3);
 
-      var bulletSpacing = fontProvider.cache['Roboto'].normal.widthOfString('9. ', 12);
+      var bulletSpacing = fontProvider.fontWrappers['Roboto'].normal.widthOfString(DEFAULT_BULLET_SPACER, 12);
 
       assert.deepEqual(_.map(_.map(lines, 'item'), 'x'), [
 				startX,
@@ -560,7 +562,7 @@ describe('Integration Test', function () {
       assert.deepEqual(getColumnText(lines, {cell:  0}), 'C1');
       assert.deepEqual(getColumnText(lines, {cell:  1}), 'Column 2');
 
-			var autoWidth = fontProvider.cache['Roboto'].normal.widthOfString('Column 2', 12);
+			var autoWidth = fontProvider.fontWrappers['Roboto'].normal.widthOfString('Column 2', 12);
 			assert.equal(lines[1].item.maxWidth, autoWidth)
 		});
 
@@ -613,5 +615,256 @@ describe('Integration Test', function () {
       assert.deepEqual(getColumnText(lines, {cell:  0}), 'Row 1 with rowspan 2');
       assert.deepEqual(getColumnText(lines, {cell:  1}), 'Row 2');
 		});
+	});
+
+	describe('lists', function () {
+		function getBulletListLine(pages, options) {
+			options.itemNumber = options.itemNumber || 1;
+
+			var bullet = pages[options.pageNumber].items[options.itemNumber * 2 - 1];
+			var content = pages[options.pageNumber].items[options.itemNumber * 2 - 2];
+
+      return {
+				bullet: bullet && bullet.item,
+				content: content && content.item
+			}
+		}
+
+		function getBulletSpacing(inlines) {
+			return fontProvider.fontWrappers['Roboto'].normal.widthOfString(inlines, 12);
+		}
+
+		it('renders a ordered list', function () {
+			var dd = {
+				content: {
+					ol: [
+						'item 1',
+						'item 2',
+						'item 3 - Paleo American Apparel forage whatever.'
+					]
+				}
+			};
+
+			var pages = renderPages('A6', dd);
+
+			assert.equal(pages.length, 1);
+
+			var item1 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 1});
+			assert.equal(_.map(item1.content.inlines, 'text').join(''), 'item 1');
+			assert.equal(_.map(item1.bullet.inlines, 'text').join(''), '1. ');
+
+			assert.equal(item1.bullet.x, MARGINS.left);
+			assert.equal(item1.content.x, MARGINS.left + getBulletSpacing('1. '));
+			assert.equal(item1.bullet.y, MARGINS.top);
+			assert.equal(item1.content.y, MARGINS.top);
+
+			var item2 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 2});
+			assert.equal(_.map(item2.content.inlines, 'text').join(''), 'item 2');
+			assert.equal(_.map(item2.bullet.inlines, 'text').join(''), '2. ');
+
+			assert.equal(item2.bullet.x, MARGINS.left);
+			assert.equal(item2.content.x, MARGINS.left + getBulletSpacing('2. '));
+			assert.equal(item2.bullet.y, MARGINS.top + LINE_HEIGHT);
+			assert.equal(item2.content.y, MARGINS.top + LINE_HEIGHT);
+
+			var item3 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 3});
+			assert.equal(_.map(item3.content.inlines, 'text').join(''), 'item 3 - Paleo American Apparel ');
+			assert.equal(_.map(item3.bullet.inlines, 'text').join(''), '3. ');
+
+			assert.equal(item3.bullet.x, MARGINS.left);
+			assert.equal(item3.content.x, MARGINS.left + getBulletSpacing('3. '));
+			assert.equal(item3.bullet.y, MARGINS.top + LINE_HEIGHT * 2);
+			assert.equal(item3.content.y, MARGINS.top + LINE_HEIGHT * 2);
+
+			var item4 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 4});
+			assert.equal(_.map(item4.content.inlines, 'text').join(''), 'forage whatever.');
+			assert.equal(item4.bullet, undefined);
+
+      assert.equal(item4.content.x, MARGINS.left + getBulletSpacing(DEFAULT_BULLET_SPACER));
+			assert.equal(item4.content.y, MARGINS.top + LINE_HEIGHT * 3);
+		});
+
+		it('renders a ordered list and adapts margin to longest list number', function () {
+			var dd = {
+				content: {
+					ol: [
+						'item 1',
+						'item 2',
+						'item 3',
+						'item 4',
+						'item 5',
+						'item 6',
+						'item 7',
+						'item 8',
+						'item 9',
+						'item 10'
+					]
+				}
+			};
+
+			var pages = renderPages('A6', dd);
+
+			assert.equal(pages.length, 1);
+
+			var item1 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 1});
+			assert.equal(_.map(item1.content.inlines, 'text').join(''), 'item 1');
+			assert.equal(_.map(item1.bullet.inlines, 'text').join(''), '1. ');
+
+			assert.equal(item1.bullet.x, MARGINS.left);
+			assert.equal(item1.content.x, MARGINS.left + getBulletSpacing('10. '));
+
+			var item10 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 10});
+			assert.equal(_.map(item10.content.inlines, 'text').join(''), 'item 10');
+			assert.equal(_.map(item10.bullet.inlines, 'text').join(''), '10. ');
+
+			assert.equal(item10.bullet.x, MARGINS.left);
+			assert.equal(item10.content.x, MARGINS.left + getBulletSpacing('10. '));
+		});
+
+		it('renders a unordered list', function () {
+			var dd = {
+				content: {
+					ul: [
+						'item 1',
+						'item 2',
+						'item 3 - Paleo American Apparel forage whatever.'
+					]
+				}
+			};
+
+			var pages = renderPages('A6', dd);
+
+			assert.equal(pages.length, 1);
+
+			var bulletRadius = 2;
+			var bulletMargin = MARGINS.left + bulletRadius;
+
+			var item1 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 1});
+			assert.equal(_.map(item1.content.inlines, 'text').join(''), 'item 1');
+			assert.equal(item1.bullet.type, 'ellipse');
+
+      assert.equal(item1.bullet.x, bulletMargin);
+			assert.equal(item1.content.x, MARGINS.left + getBulletSpacing(DEFAULT_BULLET_SPACER));
+			assert.equal(item1.content.y, MARGINS.top);
+
+			var item2 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 2});
+			assert.equal(_.map(item2.content.inlines, 'text').join(''), 'item 2');
+			assert.equal(item2.bullet.type, 'ellipse');
+
+			assert.equal(item2.bullet.x, bulletMargin);
+			assert.equal(item2.content.x, MARGINS.left + getBulletSpacing(DEFAULT_BULLET_SPACER));
+
+			var item3 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 3});
+			assert.equal(_.map(item3.content.inlines, 'text').join(''), 'item 3 - Paleo American Apparel ');
+			assert.equal(item3.bullet.type, 'ellipse');
+
+			assert.equal(item3.bullet.x, bulletMargin);
+			assert.equal(item3.content.x, MARGINS.left + getBulletSpacing(DEFAULT_BULLET_SPACER));
+			assert.equal(item3.content.y, MARGINS.top + LINE_HEIGHT * 2);
+
+			var item4 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 4});
+			assert.equal(_.map(item4.content.inlines, 'text').join(''), 'forage whatever.');
+			assert.equal(item4.bullet, undefined);
+
+			assert.equal(item4.content.x, MARGINS.left + getBulletSpacing(DEFAULT_BULLET_SPACER));
+			assert.equal(item4.content.y, MARGINS.top + LINE_HEIGHT * 3);
+		});
+
+		it('renders a unordered list and keeps constant small margin', function () {
+			var dd = {
+				content: {
+					ul: [
+						'item 1',
+						'item 2',
+						'item 3',
+						'item 4',
+						'item 5',
+						'item 6',
+						'item 7',
+						'item 8',
+						'item 9',
+						'item 10'
+					]
+				}
+			};
+
+			var pages = renderPages('A6', dd);
+
+			assert.equal(pages.length, 1);
+
+			var bulletRadius = 2;
+			var bulletMargin = MARGINS.left + bulletRadius;
+
+			var item1 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 1});
+			assert.equal(_.map(item1.content.inlines, 'text').join(''), 'item 1');
+
+			assert.equal(item1.bullet.x, bulletMargin);
+			assert.equal(item1.content.x, MARGINS.left + getBulletSpacing(DEFAULT_BULLET_SPACER));
+
+			var item10 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 10});
+			assert.equal(_.map(item10.content.inlines, 'text').join(''), 'item 10');
+
+			assert.equal(item10.bullet.x, bulletMargin);
+			assert.equal(item10.content.x, MARGINS.left + getBulletSpacing(DEFAULT_BULLET_SPACER));
+		});
+
+		it('renders nested lists', function () {
+			var dd = {
+				content: {
+					ol: [
+						'item 1',
+						[
+							'item 2',
+							{ ol: [ 'subitem 1', 'subitem 2' ] }
+						],
+						'item 3'
+					]
+				}
+			};
+
+			var pages = renderPages('A6', dd);
+
+			assert.equal(pages.length, 1);
+
+			var item1 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 1});
+			assert.equal(_.map(item1.content.inlines, 'text').join(''), 'item 1');
+			assert.equal(_.map(item1.bullet.inlines, 'text').join(''), '1. ');
+
+			assert.equal(item1.bullet.x, MARGINS.left);
+			assert.equal(item1.content.x, MARGINS.left + getBulletSpacing('1. '));
+
+			var item2 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 2});
+			assert.equal(_.map(item2.content.inlines, 'text').join(''), 'item 2');
+			assert.equal(_.map(item2.bullet.inlines, 'text').join(''), '2. ');
+
+			var item2BulletSpacing = MARGINS.left + getBulletSpacing('2. ');
+			assert.equal(item2.bullet.x, MARGINS.left);
+			assert.equal(item2.content.x, item2BulletSpacing);
+
+			var subItem1 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 3});
+			assert.equal(_.map(subItem1.content.inlines, 'text').join(''), 'subitem 1');
+			assert.equal(_.map(subItem1.bullet.inlines, 'text').join(''), '1. ');
+
+      assert.equal(subItem1.bullet.x, item2BulletSpacing);
+			assert.equal(subItem1.content.x, item2BulletSpacing + getBulletSpacing('1. '));
+			assert.equal(subItem1.content.y, MARGINS.top + LINE_HEIGHT * 2);
+
+			var subItem2 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 4});
+			assert.equal(_.map(subItem2.content.inlines, 'text').join(''), 'subitem 2');
+			assert.equal(_.map(subItem2.bullet.inlines, 'text').join(''), '2. ');
+
+			assert.equal(subItem2.bullet.x, item2BulletSpacing);
+			assert.equal(subItem2.content.x, item2BulletSpacing + getBulletSpacing('2. '));
+
+			var item3 = getBulletListLine(pages, {pageNumber: 0, itemNumber: 5});
+			assert.equal(_.map(item3.content.inlines, 'text').join(''), 'item 3');
+			assert.equal(_.map(item3.bullet.inlines, 'text').join(''), '3. ');
+
+			var item3BulletSpacing = MARGINS.left + getBulletSpacing('3. ');
+			assert.equal(item3.bullet.x, MARGINS.left);
+			assert.equal(item3.content.x, item3BulletSpacing);
+			assert.equal(item3.content.y, MARGINS.top + LINE_HEIGHT * 4);
+		});
+
 	});
 });
