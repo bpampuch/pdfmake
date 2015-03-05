19a0770fb596e2a257efcb7ec0c617d1f3bdf277
Birgitta B <bboeckel@thoughtworks.com>
2015-03-05 18:57:40 +0100

    Splits up integration test into multiple files

---
+++ tests/integration/background.js
@@ -0,0 +1,40 @@
+var assert = require('assert');
+var _ = require('lodash');
+var sizes = require('../../src/standardPageSizes');
+
+var integrationTestHelper = require('./integrationTestHelper');
+
+describe('Integration test: background', function () {
+
+	var testHelper = new integrationTestHelper();
+
+	it('renders on every page', function () {
+		var dd = {
+			background: function (page) {
+				return [
+					'Background paragraph on page ' + page
+				]
+			},
+			content: [
+				'First page',
+				'\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n',
+				'Another Page'
+			]
+		};
+
+		var pages = testHelper.renderPages('A6', dd);
+
+		assert.equal(pages.length, 2);
+
+		var backgroundPage1 = pages[0].items[0].item;
+		assert.equal(_.map(backgroundPage1.inlines, 'text').join(''), 'Background paragraph on page 1');
+		assert.equal(backgroundPage1.x, 0);
+		assert.equal(backgroundPage1.y, 0);
+
+		var backgroundPage2 = pages[1].items[0].item;
+		assert.equal(_.map(backgroundPage2.inlines, 'text').join(''), 'Background paragraph on page 2');
+		assert.equal(backgroundPage2.x, 0);
+		assert.equal(backgroundPage2.y, 0);
+	});
+
+});
---
+++ tests/integration/images.js
@@ -0,0 +1,67 @@
+var assert = require('assert');
+var _ = require('lodash');
+var sizes = require('../../src/standardPageSizes');
+
+var integrationTestHelper = require('./integrationTestHelper');
+
+describe('Integration Test: images', function () {
+
+	var testHelper = new integrationTestHelper();
+
+	var INLINE_TEST_IMAGE = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAGAQMAAADNIO3CAAAAA1BMVEUAAN7GEcIJAAAAAWJLR0QAiAUdSAAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB98DBREbA3IZ3d8AAAALSURBVAjXY2BABwAAEgAB74lUpAAAAABJRU5ErkJggg==';
+
+	describe('basics', function () {
+		it('renders next element below image', function () {
+			var imageHeight = 150;
+			var dd = {
+				content: [
+					{
+						image: INLINE_TEST_IMAGE,
+						height: imageHeight
+					},
+					'some Text'
+				]
+			};
+
+			var pages = testHelper.renderPages('A6', dd);
+
+			assert.equal(pages.length, 1);
+
+			var image = pages[0].items[0].item;
+			var someElementAfterImage = pages[0].items[1].item;
+
+			assert.equal(image.x, testHelper.MARGINS.left);
+			assert.equal(image.y, testHelper.MARGINS.top);
+			assert.equal(someElementAfterImage.x, testHelper.MARGINS.left);
+			assert.equal(someElementAfterImage.y, testHelper.MARGINS.top + imageHeight);
+		});
+
+		it('renders image below text', function () {
+			var imageHeight = 150;
+			var dd = {
+				content: [
+					'some Text',
+					{
+						image: INLINE_TEST_IMAGE,
+						height: imageHeight
+					}
+				]
+			};
+
+			var pages = testHelper.renderPages('A6', dd);
+
+			assert.equal(pages.length, 1);
+
+			var someElementAfterImage = pages[0].items[0].item;
+			var image = pages[0].items[1].item;
+
+
+			assert.equal(someElementAfterImage.x, testHelper.MARGINS.left);
+			assert.equal(someElementAfterImage.y, testHelper.MARGINS.top);
+
+			assert.equal(image.x, testHelper.MARGINS.left);
+			assert.equal(image.y, testHelper.MARGINS.top + testHelper.LINE_HEIGHT);
+		});
+	});
+
+});
