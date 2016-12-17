5f4b4efee7a529e32390895d8756289bb07340da
Libor M <liborm85@gmail.com>
2016-12-17 13:53:52 +0100

    reformat

--- gulpfile.js
+++ gulpfile.js
@@ -25,24 +25,26 @@ var uglifyOptions = {
 
 
 gulp.task('default', [/*'lint',*/ 'test', 'build']);
-gulp.task('build', function() {
+gulp.task('build', function () {
 	return gulp.src('src/browser-extensions/pdfMake.js')
 		.pipe(webpack(require('./webpack.config.js'), null, reportWebPackErrors))
 		.pipe(gulp.dest('build'))
 		.pipe(sourcemaps.init())
 		.pipe(uglify(uglifyOptions))
-		.pipe(rename({ extname: '.min.js' }))
+		.pipe(rename({extname: '.min.js'}))
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest('build'));
 });
 
 function reportWebPackErrors(err, stats) {
-  if(err) throw new gutil.PluginError("webpack", err);
-  gutil.log("[webpack]", stats.toString({
-  }));
+	if (err) {
+		throw new gutil.PluginError("webpack", err);
+	}
+	gutil.log("[webpack]", stats.toString({
+	}));
 }
 
-gulp.task('test', ['prepareTestEnv'], function(cb) {
+gulp.task('test', ['prepareTestEnv'], function (cb) {
 	return gulp.src(['test-env/tests/**/*.js'])
 		.pipe(mocha({
 			debugBrk: DEBUG,
@@ -52,19 +54,19 @@ gulp.task('test', ['prepareTestEnv'], function(cb) {
 
 gulp.task('prepareTestEnv', ['copy-src-with-exposed-test-methods', 'copy-tests']);
 
-gulp.task('copy-src-with-exposed-test-methods', function() {
-	return gulp.src(['src/**/*.js'], { base: './' })
+gulp.task('copy-src-with-exposed-test-methods', function () {
+	return gulp.src(['src/**/*.js'], {base: './'})
 		.pipe(replace(/^(\/(\*)*TESTS.*$)/gm, '/$1'))
 		.pipe(gulp.dest('test-env'));
 });
 
-gulp.task('copy-tests', function() {
-	return gulp.src('tests/**/*.*', { base: './' })
+gulp.task('copy-tests', function () {
+	return gulp.src('tests/**/*.*', {base: './'})
 		.pipe(gulp.dest('test-env'))
 });
 
-gulp.task('lint', function() {
-  return gulp.src(['./src/**/*.js'])
-    .pipe(jshint())
+gulp.task('lint', function () {
+	return gulp.src(['./src/**/*.js'])
+		.pipe(jshint())
 		.pipe(jshint.reporter());
 });
