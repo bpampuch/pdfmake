1fb91c2535463e73fd5545e6957f3b516078ce71
Libor M <liborm85@gmail.com>
2016-12-18 10:19:45 +0100

    reformat

--- webpack.config.js
+++ webpack.config.js
@@ -3,52 +3,58 @@ var StringReplacePlugin = require("string-replace-webpack-plugin");
 
 
 module.exports = {
-  entry: './src/browser-extensions/pdfMake.js',
-  output: {
-    path: path.join(__dirname, './build'),
-    filename: 'pdfmake.js',
-    libraryTarget: 'umd'
-  },
-  resolve: {
-    alias: {
-      fs: path.join(__dirname, './src/browser-extensions/virtual-fs.js')
-    }
-  },
-  module: {
-    loaders: [
-      { test: /\.json$/, loader: 'json' },
-      { test: /pdfMake.js$/, loader: 'expose?pdfMake', include: [ path.join(__dirname, './src/browser-extensions')] },
-      { test: /pdfkit[\/\\]js[\/\\]mixins[\/\\]fonts.js$/, loader: StringReplacePlugin.replace({
-        replacements: [
-          {
-            pattern: 'return this.font(\'Helvetica\');',
-            replacement: function () { return ''; }
-          }
-        ]})
-      },
-      { test: /fontkit[\/\\]index.js$/, loader: StringReplacePlugin.replace({
-        replacements: [
-          {
-            pattern: /fs\./g,
-            replacement: function () { return 'require(\'fs\').'; }
-          }
-        ]})
-      },
-      { test: /readable-stream[\/\\]/, loader: StringReplacePlugin.replace({
-        replacements: [
-          {
-            pattern: 'stream.read()',
-            replacement: function () { return 'stream.read(9007199254740991)'; }
-          }
-        ]})
-      }
-    ],
-    postLoaders: [
-      { test: /fontkit[\/\\]/, loader: "transform?brfs" },
-      { test: /unicode-properties[\/\\]/, loader: "transform?brfs" }
-    ]
-  },
-  plugins: [
-    new StringReplacePlugin()
-  ]
+	entry: './src/browser-extensions/pdfMake.js',
+	output: {
+		path: path.join(__dirname, './build'),
+		filename: 'pdfmake.js',
+		libraryTarget: 'umd'
+	},
+	resolve: {
+		alias: {
+			fs: path.join(__dirname, './src/browser-extensions/virtual-fs.js')
+		}
+	},
+	module: {
+		loaders: [
+			{test: /\.json$/, loader: 'json'},
+			{test: /pdfMake.js$/, loader: 'expose?pdfMake', include: [path.join(__dirname, './src/browser-extensions')]},
+			{test: /pdfkit[\/\\]js[\/\\]mixins[\/\\]fonts.js$/, loader: StringReplacePlugin.replace({
+					replacements: [
+						{
+							pattern: 'return this.font(\'Helvetica\');',
+							replacement: function () {
+								return '';
+							}
+						}
+					]})
+			},
+			{test: /fontkit[\/\\]index.js$/, loader: StringReplacePlugin.replace({
+					replacements: [
+						{
+							pattern: /fs\./g,
+							replacement: function () {
+								return 'require(\'fs\').';
+							}
+						}
+					]})
+			},
+			{test: /readable-stream[\/\\]/, loader: StringReplacePlugin.replace({
+					replacements: [
+						{
+							pattern: 'stream.read()',
+							replacement: function () {
+								return 'stream.read(9007199254740991)';
+							}
+						}
+					]})
+			}
+		],
+		postLoaders: [
+			{test: /fontkit[\/\\]/, loader: "transform?brfs"},
+			{test: /unicode-properties[\/\\]/, loader: "transform?brfs"}
+		]
+	},
+	plugins: [
+		new StringReplacePlugin()
+	]
 };
