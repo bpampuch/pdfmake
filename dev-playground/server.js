3ecff15dbded2c73c5b0550a4ce9c0b46e7c4cc8
Jirachai Chansivanon <antronic.inc@gmail.com>
2017-03-24 16:43:27 +0700

    Fix 'CANNOT GET /' cause from express static

--- dev-playground/server.js
+++ dev-playground/server.js
@@ -11,7 +11,7 @@ var app = express();
 
 var rootDir = path.resolve(path.dirname('.'));
 
-app.use(express.static(rootDir + '/dev-playground/public/'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
