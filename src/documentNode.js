50ca80961b66931288816275c2c81d1738322a8f
Libor M <liborm85@gmail.com>
2018-02-18 15:08:32 +0100

    fix pdf write

--- src/documentNode.js
+++ src/documentNode.js
@@ -4,7 +4,8 @@ import fs from 'fs';
 export default class DocumentNode extends DocumentBase {
 
 	write(filename) {
-		this.getStream().pipe(fs.createWriteStream(filename)).end();
+		this.getStream().pipe(fs.createWriteStream(filename));
+		this.getStream().end();
 	}
 
 }
