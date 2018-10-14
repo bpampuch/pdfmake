c008e6d131b3697d17b55c6c2edefe2daf814fed
Libor M <liborm85@gmail.com>
2018-10-14 12:51:32 +0200

    implemented container and text preprocessor

---
+++ src/extensions/container/containerPreprocessor.js
@@ -0,0 +1,26 @@
+/**
+ * @mixin
+ */
+const ContainerPreprocessor = Base => class extends Base {
+
+	constructor() {
+		super();
+
+		this.registerNodeType(
+			node => 'stack' in node,
+			node => this.preprocessVerticalContainer(node)
+		);
+	}
+
+	preprocessVerticalContainer(node) {
+		let items = node.stack;
+
+		for (let i = 0, l = items.length; i < l; i++) {
+			items[i] = this.preprocessNode(items[i]);
+		}
+
+		return node;
+	}
+};
+
+export default ContainerPreprocessor;
---
+++ src/extensions/text/textPreprocessor.js
@@ -0,0 +1,29 @@
+import { isArray } from '../../helpers/variableType';
+
+/**
+ * @mixin
+ */
+const TextPreprocessor = Base => class extends Base {
+
+	constructor() {
+		super();
+
+		this.registerNodeType(
+			node => 'text' in node,
+			node => this.preprocessText(node)
+		);
+	}
+
+	preprocessText(node) {
+		if (isArray(node.text)) {
+			for (var i = 0, l = node.text.length; i < l; i++) {
+				node.text[i] = this.preprocessNode(node.text[i]);
+			}
+		}
+
+		return node;
+	}
+
+};
+
+export default TextPreprocessor;
