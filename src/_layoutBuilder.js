9ec71dce7ca8c0148e285a5380dc102698592c08
Libor M <liborm85@gmail.com>
2018-10-14 13:54:28 +0200

    reimplemented

--- src/_layoutBuilder.js
+++ src/_layoutBuilder.js
@@ -611,41 +611,7 @@ class LayoutBuilder {
 			return null;
 		}
 
-		let line = new Line(this.writer.context().availableWidth);
-		let textTools = new TextTools(null);
-
-		var isForceContinue = false;
-		while (textNode._inlines && textNode._inlines.length > 0 &&
-			(line.hasEnoughSpaceForInline(textNode._inlines[0], textNode._inlines.slice(1)) || isForceContinue)) {
-			let isHardWrap = false;
-			let inline = textNode._inlines.shift();
-			isForceContinue = false;
-
-			if (!inline.noWrap && inline.text.length > 1 && inline.width > line.getAvailableWidth()) {
-				let widthPerChar = inline.width / inline.text.length;
-				let maxChars = Math.floor(line.getAvailableWidth() / widthPerChar);
-				if (maxChars < 1) {
-					maxChars = 1;
-				}
-				if (maxChars < inline.text.length) {
-					let newInline = cloneInline(inline);
-
-					newInline.text = inline.text.substr(maxChars);
-					inline.text = inline.text.substr(0, maxChars);
-
-					newInline.width = textTools.widthOfString(newInline.text, newInline.font, newInline.fontSize, newInline.characterSpacing, newInline.fontFeatures);
-					inline.width = textTools.widthOfString(inline.text, inline.font, inline.fontSize, inline.characterSpacing, inline.fontFeatures);
-
-					textNode._inlines.unshift(newInline);
-					isHardWrap = true;
-				}
-			}
-
-			line.addInline(inline);
-			isForceContinue = inline.noNewLine && !isHardWrap;
-		}
-
-		line.lastLineInParagraph = textNode._inlines.length === 0;
+		// ...
 
 		return line;
 	}
