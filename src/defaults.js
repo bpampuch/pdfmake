9717e36ded1b4c57d3a2ca43772eedf0dfc2e49e
Libor M <liborm85@gmail.com>
2018-10-14 12:35:05 +0200

    added class for storing default values

---
+++ src/defaults.js
@@ -0,0 +1,19 @@
+export default {
+	font: 'Roboto',
+	fontSize: 12,
+	fontFeatures: null,
+	bold: false,
+	italics: false,
+	color: black,
+	decoration: null,
+	decorationColor: null,
+	decorationStyle: null,
+	background: null,
+	lineHeight: 1,
+	characterSpacing: 0,
+	link: null,
+	linkToPage: null,
+	noWrap: false,
+	preserveLeadingSpaces: false,
+	alignment: 'left'
+}
