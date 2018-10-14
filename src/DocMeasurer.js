edcf065f79341bf2223cd0fe430e82916e4fc288
Libor M <liborm85@gmail.com>
2018-10-14 13:11:09 +0200

    DocMeasurer - simple implementation

--- src/DocMeasurer.js
+++ src/DocMeasurer.js
@@ -1,6 +1,84 @@
 import { processAllExtenstionsByCondition, processFirstExtenstionsByCondition } from './helpers/extensionsRunner';
+import { isNumber, isArray, isUndefined } from './helpers/variableType';
 import TextInlines from './textInlines';
 import StyleContextStack from './styleContextStack';
+
+const getNodeMargin = node => {
+	const processSingleMargins = (node, currentMargin) => {
+		if (node.marginLeft || node.marginTop || node.marginRight || node.marginBottom) {
+			return [
+				node.marginLeft || currentMargin[0] || 0,
+				node.marginTop || currentMargin[1] || 0,
+				node.marginRight || currentMargin[2] || 0,
+				node.marginBottom || currentMargin[3] || 0
+			];
+		}
+		return currentMargin;
+	}
+
+	const flattenStyleArray = styleArray => {
+		let flattenedStyles = {};
+		for (let i = styleArray.length - 1; i >= 0; i--) {
+			let styleName = styleArray[i];
+			let style = self.styleStack.styleDictionary[styleName];
+			for (let key in style) {
+				if (style.hasOwnProperty(key)) {
+					flattenedStyles[key] = style[key];
+				}
+			}
+		}
+		return flattenedStyles;
+	}
+
+	const convertMargin = margin => {
+		if (isNumber(margin)) {
+			margin = [margin, margin, margin, margin];
+		} else if (isArray(margin)) {
+			if (margin.length === 2) {
+				margin = [margin[0], margin[1], margin[0], margin[1]];
+			}
+		}
+		return margin;
+	}
+
+	let margin = [undefined, undefined, undefined, undefined];
+
+	if (node.style) {
+		let styleArray = isArray(node.style) ? node.style : [node.style];
+		let flattenedStyleArray = flattenStyleArray(styleArray);
+
+		if (flattenedStyleArray) {
+			margin = processSingleMargins(flattenedStyleArray, margin);
+		}
+
+		if (flattenedStyleArray.margin) {
+			margin = convertMargin(flattenedStyleArray.margin);
+		}
+	}
+
+	margin = processSingleMargins(node, margin);
+
+	if (node.margin) {
+		margin = convertMargin(node.margin);
+	}
+
+	if (isUndefined(margin[0]) && isUndefined(margin[1]) && isUndefined(margin[2]) && isUndefined(margin[3])) {
+		return null;
+	}
+
+	return margin;
+};
+
+const extendMargins = node => {
+	let margin = node._margin;
+
+	if (margin) {
+		node._minWidth += margin[0] + margin[2];
+		node._maxWidth += margin[0] + margin[2];
+	}
+
+	return node;
+};
 
 class DocMeasurer {
 
@@ -31,7 +109,15 @@ class DocMeasurer {
 	}
 
 	measureNode(node) {
-		node = processFirstExtenstionsByCondition(this.nodeTypes, node);
+		return this.styleStack.auto(node, () => {
+			// TODO: refactor + rethink whether this is the proper way to handle margins
+			node._margin = getNodeMargin(node);
+
+			node = processFirstExtenstionsByCondition(this.nodeTypes, node);
+
+			return extendMargins(node);
+		});
+
 		node = processAllExtenstionsByCondition(this.properties, node);
 
 		return node;
--- src/_docMeasure.js
+++ src/_docMeasure.js
@@ -31,8 +31,7 @@ class DocMeasure {
 		var self = this;
 
 		return this.styleStack.auto(node, () => {
-			// TODO: refactor + rethink whether this is the proper way to handle margins
-			node._margin = getNodeMargin(node);
+			// ...
 
 			if (node.columns) {
 				return extendMargins(self.measureColumns(node));
@@ -59,83 +58,7 @@ class DocMeasure {
 			}
 		});
 
-		function extendMargins(node) {
-			let margin = node._margin;
-
-			if (margin) {
-				node._minWidth += margin[0] + margin[2];
-				node._maxWidth += margin[0] + margin[2];
-			}
-
-			return node;
-		}
-
-		function getNodeMargin() {
-
-			function processSingleMargins(node, currentMargin) {
-				if (node.marginLeft || node.marginTop || node.marginRight || node.marginBottom) {
-					return [
-						node.marginLeft || currentMargin[0] || 0,
-						node.marginTop || currentMargin[1] || 0,
-						node.marginRight || currentMargin[2] || 0,
-						node.marginBottom || currentMargin[3] || 0
-					];
-				}
-				return currentMargin;
-			}
-
-			function flattenStyleArray(styleArray) {
-				let flattenedStyles = {};
-				for (let i = styleArray.length - 1; i >= 0; i--) {
-					let styleName = styleArray[i];
-					let style = self.styleStack.styleDictionary[styleName];
-					for (let key in style) {
-						if (style.hasOwnProperty(key)) {
-							flattenedStyles[key] = style[key];
-						}
-					}
-				}
-				return flattenedStyles;
-			}
-
-			function convertMargin(margin) {
-				if (isNumber(margin)) {
-					margin = [margin, margin, margin, margin];
-				} else if (isArray(margin)) {
-					if (margin.length === 2) {
-						margin = [margin[0], margin[1], margin[0], margin[1]];
-					}
-				}
-				return margin;
-			}
-
-			let margin = [undefined, undefined, undefined, undefined];
-
-			if (node.style) {
-				let styleArray = isArray(node.style) ? node.style : [node.style];
-				let flattenedStyleArray = flattenStyleArray(styleArray);
-
-				if (flattenedStyleArray) {
-					margin = processSingleMargins(flattenedStyleArray, margin);
-				}
-
-				if (flattenedStyleArray.margin) {
-					margin = convertMargin(flattenedStyleArray.margin);
-				}
-			}
-
-			margin = processSingleMargins(node, margin);
-
-			if (node.margin) {
-				margin = convertMargin(node.margin);
-			}
-
-			if (margin[0] === undefined && margin[1] === undefined && margin[2] === undefined && margin[3] === undefined) {
-				return null;
-			} else {
-				return margin;
-			}
-		}
+		// ...
 	}
 
 	convertIfBase64Image(node) {
