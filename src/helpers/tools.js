96203e5bbeae59b11445bb1a092d19e2f324d06d
Libor M <liborm85@gmail.com>
2018-10-14 13:54:58 +0200

    next functions from helpers rewrited to helpers/tools

--- src/helpers/tools.js
+++ src/helpers/tools.js
@@ -7,3 +7,43 @@ export function addAll(target, otherArray) {
 		target.push(item);
 	});
 }
+
+export function pack() {
+	var result = {};
+
+	for (var i = 0, l = arguments.length; i < l; i++) {
+		var obj = arguments[i];
+
+		if (obj) {
+			for (var key in obj) {
+				if (obj.hasOwnProperty(key)) {
+					result[key] = obj[key];
+				}
+			}
+		}
+	}
+
+	return result;
+}
+
+export function offsetVector(vector, x, y) {
+	switch (vector.type) {
+		case 'ellipse':
+		case 'rect':
+			vector.x += x;
+			vector.y += y;
+			break;
+		case 'line':
+			vector.x1 += x;
+			vector.x2 += x;
+			vector.y1 += y;
+			vector.y2 += y;
+			break;
+		case 'polyline':
+			for (var i = 0, l = vector.points.length; i < l; i++) {
+				vector.points[i].x += x;
+				vector.points[i].y += y;
+			}
+			break;
+	}
+}
