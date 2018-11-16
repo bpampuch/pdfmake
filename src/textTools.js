d90943cadfa48a110710a66d951589ca5654661a
Dorothy Thurston <dorothy.thurston@autodesk.com>
2018-11-15 19:06:49 -0800

    Update textTools.js

--- src/textTools.js
+++ src/textTools.js
@@ -1,108 +1,17 @@
 'use strict';
 
-var isString = require('./helpers').isString;
-var isNumber = require('./helpers').isNumber;
-var isObject = require('./helpers').isObject;
-var isArray = require('./helpers').isArray;
-var isUndefined = require('./helpers').isUndefined;
-var LineBreaker = require('linebreak');
+const isString = require('./helpers').isString;
+const isNumber = require('./helpers').isNumber;
+const isObject = require('./helpers').isObject;
+const isArray = require('./helpers').isArray;
+const isUndefined = require('./helpers').isUndefined;
+const LineBreaker = require('linebreak');
 
-var LEADING = /^(\s)+/g;
-var TRAILING = /(\s)+$/g;
-
-/**
- * Creates an instance of TextTools - text measurement utility
- *
- * @constructor
- * @param {FontProvider} fontProvider
- */
-function TextTools(fontProvider) {
-	this.fontProvider = fontProvider;
-}
-
-/**
- * Converts an array of strings (or inline-definition-objects) into a collection
- * of inlines and calculated minWidth/maxWidth.
- * and their min/max widths
- * @param  {Object} textArray - an array of inline-definition-objects (or strings)
- * @param  {Object} styleContextStack current style stack
- * @return {Object}                   collection of inlines, minWidth, maxWidth
- */
-TextTools.prototype.buildInlines = function (textArray, styleContextStack) {
-	var measured = measure(this.fontProvider, textArray, styleContextStack);
-
-	var minWidth = 0,
-		maxWidth = 0,
-		currentLineWidth;
-
-	measured.forEach(function (inline) {
-		minWidth = Math.max(minWidth, inline.width - inline.leadingCut - inline.trailingCut);
-
-		if (!currentLineWidth) {
-			currentLineWidth = {width: 0, leadingCut: inline.leadingCut, trailingCut: 0};
-		}
-
-		currentLineWidth.width += inline.width;
-		currentLineWidth.trailingCut = inline.trailingCut;
-
-		maxWidth = Math.max(maxWidth, getTrimmedWidth(currentLineWidth));
-
-		if (inline.lineEnd) {
-			currentLineWidth = null;
-		}
-	});
-
-	if (getStyleProperty({}, styleContextStack, 'noWrap', false)) {
-		minWidth = maxWidth;
-	}
-
-	return {
-		items: measured,
-		minWidth: minWidth,
-		maxWidth: maxWidth
-	};
-
-	function getTrimmedWidth(item) {
-		return Math.max(0, item.width - item.leadingCut - item.trailingCut);
-	}
-};
-
-/**
- * Returns size of the specified string (without breaking it) using the current style
- * @param  {String} text              text to be measured
- * @param  {Object} styleContextStack current style stack
- * @return {Object}                   size of the specified string
- */
-TextTools.prototype.sizeOfString = function (text, styleContextStack) {
-	text = text ? text.toString().replace(/\t/g, '    ') : '';
-
-	//TODO: refactor - extract from measure
-	var fontName = getStyleProperty({}, styleContextStack, 'font', 'Roboto');
-	var fontSize = getStyleProperty({}, styleContextStack, 'fontSize', 12);
-	var fontFeatures = getStyleProperty({}, styleContextStack, 'fontFeatures', null);
-	var bold = getStyleProperty({}, styleContextStack, 'bold', false);
-	var italics = getStyleProperty({}, styleContextStack, 'italics', false);
-	var lineHeight = getStyleProperty({}, styleContextStack, 'lineHeight', 1);
-	var characterSpacing = getStyleProperty({}, styleContextStack, 'characterSpacing', 0);
-
-	var font = this.fontProvider.provideFont(fontName, bold, italics);
-
-	return {
-		width: widthOfString(text, font, fontSize, characterSpacing, fontFeatures),
-		height: font.lineHeight(fontSize) * lineHeight,
-		fontSize: fontSize,
-		lineHeight: lineHeight,
-		ascender: font.ascender / 1000 * fontSize,
-		descender: font.descender / 1000 * fontSize
-	};
-};
-
-TextTools.prototype.widthOfString = function (text, font, fontSize, characterSpacing, fontFeatures) {
-	return widthOfString(text, font, fontSize, characterSpacing, fontFeatures);
-};
+const LEADING = /^(\s)+/g;
+const TRAILING = /(\s)+$/g;
 
 function splitWords(text, noWrap) {
-	var results = [];
+	const results = [];
 	text = text.replace(/\t/g, '    ');
 
 	if (noWrap) {
@@ -110,12 +19,12 @@ function splitWords(text, noWrap) {
 		return results;
 	}
 
-	var breaker = new LineBreaker(text);
-	var last = 0;
-	var bk;
+	const breaker = new LineBreaker(text);
+	let last = 0;
+	let bk;
 
 	while (bk = breaker.nextBreak()) {
-		var word = text.slice(last, bk.position);
+		let word = text.slice(last, bk.position);
 
 		if (bk.required || word.match(/\r?\n$|\r$/)) { // new line
 			word = word.replace(/\r?\n$|\r$/, '');
@@ -134,7 +43,7 @@ function copyStyle(source, destination) {
 	destination = destination || {};
 	source = source || {}; //TODO: default style
 
-	for (var key in source) {
+	for (let key in source) {
 		if (key != 'text' && source.hasOwnProperty(key)) {
 			destination[key] = source[key];
 		}
@@ -146,8 +55,8 @@ function copyStyle(source, destination) {
 function normalizeTextArray(array, styleContextStack) {
 	function flatten(array) {
 		return array.reduce(function (prev, cur) {
-			var current = isArray(cur.text) ? flatten(cur.text) : cur;
-			var more = [].concat(current).some(Array.isArray);
+			const current = isArray(cur.text) ? flatten(cur.text) : cur;
+			const more = [].concat(current).some(Array.isArray);
 			return prev.concat(more ? flatten(current) : current);
 		}, []);
 	}
@@ -161,10 +70,10 @@ function normalizeTextArray(array, styleContextStack) {
 			return null;
 		}
 
-		var word = words[index].text;
+		let word = words[index].text;
 
 		if (noWrap) {
-			var tmpWords = splitWords(normalizeString(word), false);
+			const tmpWords = splitWords(normalizeString(word), false);
 			if (isUndefined(tmpWords[tmpWords.length - 1])) {
 				return null;
 			}
@@ -174,7 +83,7 @@ function normalizeTextArray(array, styleContextStack) {
 		return word;
 	}
 
-	var results = [];
+	const results = [];
 
 	if (!isArray(array)) {
 		array = [array];
@@ -182,13 +91,13 @@ function normalizeTextArray(array, styleContextStack) {
 
 	array = flatten(array);
 
-	var lastWord = null;
-	for (var i = 0, l = array.length; i < l; i++) {
-		var item = array[i];
-		var style = null;
-		var words;
+	let lastWord = null;
+	for (let i = 0, l = array.length; i < l; i++) {
+		const item = array[i];
+		let style = null;
+		let words;
 
-		var noWrap = getStyleProperty(item || {}, styleContextStack, 'noWrap', false);
+		const noWrap = getStyleProperty(item || {}, styleContextStack, 'noWrap', false);
 		if (isObject(item)) {
 			if (item._textRef && item._textRef._textNodeRef.text) {
 				item.text = item._textRef._textNodeRef.text;
@@ -200,16 +109,16 @@ function normalizeTextArray(array, styleContextStack) {
 		}
 
 		if (lastWord && words.length) {
-			var firstWord = getOneWord(0, words, noWrap);
+			const firstWord = getOneWord(0, words, noWrap);
 
-			var wrapWords = splitWords(normalizeString(lastWord + firstWord), false);
+			const wrapWords = splitWords(normalizeString(lastWord + firstWord), false);
 			if (wrapWords.length === 1) {
 				results[results.length - 1].noNewLine = true;
 			}
 		}
 
-		for (var i2 = 0, l2 = words.length; i2 < l2; i2++) {
-			var result = {
+		for (let i2 = 0, l2 = words.length; i2 < l2; i2++) {
+			const result = {
 				text: words[i2].text
 			};
 
@@ -231,6 +140,8 @@ function normalizeTextArray(array, styleContextStack) {
 	return results;
 }
 
+
+
 function normalizeString(value) {
 	if (value === undefined || value === null) {
 		return '';
@@ -244,7 +155,7 @@ function normalizeString(value) {
 }
 
 function getStyleProperty(item, styleContextStack, property, defaultValue) {
-	var value;
+	let value;
 
 	if (item[property] !== undefined && item[property] !== null) {
 		// item defines this property
@@ -267,10 +178,10 @@ function getStyleProperty(item, styleContextStack, property, defaultValue) {
 }
 
 function measure(fontProvider, textArray, styleContextStack) {
-	var normalized = normalizeTextArray(textArray, styleContextStack);
+	const normalized = normalizeTextArray(textArray, styleContextStack);
 
 	if (normalized.length) {
-		var leadingIndent = getStyleProperty(normalized[0], styleContextStack, 'leadingIndent', 0);
+		const leadingIndent = getStyleProperty(normalized[0], styleContextStack, 'leadingIndent', 0);
 
 		if (leadingIndent) {
 			normalized[0].leadingCut = -leadingIndent;
@@ -279,25 +190,25 @@ function measure(fontProvider, textArray, styleContextStack) {
 	}
 
 	normalized.forEach(function (item) {
-		var fontName = getStyleProperty(item, styleContextStack, 'font', 'Roboto');
-		var fontSize = getStyleProperty(item, styleContextStack, 'fontSize', 12);
-		var fontFeatures = getStyleProperty(item, styleContextStack, 'fontFeatures', null);
-		var bold = getStyleProperty(item, styleContextStack, 'bold', false);
-		var italics = getStyleProperty(item, styleContextStack, 'italics', false);
-		var color = getStyleProperty(item, styleContextStack, 'color', 'black');
-		var decoration = getStyleProperty(item, styleContextStack, 'decoration', null);
-		var decorationColor = getStyleProperty(item, styleContextStack, 'decorationColor', null);
-		var decorationStyle = getStyleProperty(item, styleContextStack, 'decorationStyle', null);
-		var background = getStyleProperty(item, styleContextStack, 'background', null);
-		var lineHeight = getStyleProperty(item, styleContextStack, 'lineHeight', 1);
-		var characterSpacing = getStyleProperty(item, styleContextStack, 'characterSpacing', 0);
-		var link = getStyleProperty(item, styleContextStack, 'link', null);
-		var linkToPage = getStyleProperty(item, styleContextStack, 'linkToPage', null);
-		var noWrap = getStyleProperty(item, styleContextStack, 'noWrap', null);
-		var preserveLeadingSpaces = getStyleProperty(item, styleContextStack, 'preserveLeadingSpaces', false);
-		var preserveTrailingSpaces = getStyleProperty(item, styleContextStack, 'preserveTrailingSpaces', false);
+		const fontName = getStyleProperty(item, styleContextStack, 'font', 'Roboto');
+		const fontSize = getStyleProperty(item, styleContextStack, 'fontSize', 12);
+		const fontFeatures = getStyleProperty(item, styleContextStack, 'fontFeatures', null);
+		const bold = getStyleProperty(item, styleContextStack, 'bold', false);
+		const italics = getStyleProperty(item, styleContextStack, 'italics', false);
+		const color = getStyleProperty(item, styleContextStack, 'color', 'black');
+		const decoration = getStyleProperty(item, styleContextStack, 'decoration', null);
+		const decorationColor = getStyleProperty(item, styleContextStack, 'decorationColor', null);
+		const decorationStyle = getStyleProperty(item, styleContextStack, 'decorationStyle', null);
+		const background = getStyleProperty(item, styleContextStack, 'background', null);
+		const lineHeight = getStyleProperty(item, styleContextStack, 'lineHeight', 1);
+		const characterSpacing = getStyleProperty(item, styleContextStack, 'characterSpacing', 0);
+		const link = getStyleProperty(item, styleContextStack, 'link', null);
+		const linkToPage = getStyleProperty(item, styleContextStack, 'linkToPage', null);
+		const noWrap = getStyleProperty(item, styleContextStack, 'noWrap', null);
+		const preserveLeadingSpaces = getStyleProperty(item, styleContextStack, 'preserveLeadingSpaces', false);
+		const preserveTrailingSpaces = getStyleProperty(item, styleContextStack, 'preserveTrailingSpaces', false);
 
-		var font = fontProvider.provideFont(fontName, bold, italics);
+		const font = fontProvider.provideFont(fontName, bold, italics);
 
 		item.width = widthOfString(item.text, font, fontSize, characterSpacing, fontFeatures);
 		item.height = font.lineHeight(fontSize) * lineHeight;
@@ -306,12 +217,12 @@ function measure(fontProvider, textArray, styleContextStack) {
 			item.leadingCut = 0;
 		}
 
-		var leadingSpaces;
+		let leadingSpaces;
 		if (!preserveLeadingSpaces && (leadingSpaces = item.text.match(LEADING))) {
 			item.leadingCut += widthOfString(leadingSpaces[0], font, fontSize, characterSpacing, fontFeatures);
 		}
 
-		var trailingSpaces;
+		let trailingSpaces;
 		if (!preserveTrailingSpaces && (trailingSpaces = item.text.match(TRAILING))) {
 			item.trailingCut = widthOfString(trailingSpaces[0], font, fontSize, characterSpacing, fontFeatures);
 		} else {
@@ -338,6 +249,97 @@ function measure(fontProvider, textArray, styleContextStack) {
 
 function widthOfString(text, font, fontSize, characterSpacing, fontFeatures) {
 	return font.widthOfString(text, fontSize, fontFeatures) + ((characterSpacing || 0) * (text.length - 1));
+}
+
+class TextTools {
+	/**
+	 * Creates an instance of TextTools - text measurement utility
+	 *
+	 * @constructor
+	 * @param {FontProvider} fontProvider
+	 */
+	constructor(fontProvider) {
+		this.fontProvider = fontProvider;
+	}
+	/**
+	 * Converts an array of strings (or inline-definition-objects) into a collection
+	 * of inlines and calculated minWidth/maxWidth.
+	 * and their min/max widths
+	 * @param  {Object} textArray - an array of inline-definition-objects (or strings)
+	 * @param  {Object} styleContextStack current style stack
+	 * @return {Object}                   collection of inlines, minWidth, maxWidth
+	 */
+	buildInlines(textArray, styleContextStack) {
+		const measured = measure(this.fontProvider, textArray, styleContextStack);
+	
+		let minWidth = 0,
+			maxWidth = 0,
+			currentLineWidth;
+	
+		measured.forEach(function (inline) {
+			minWidth = Math.max(minWidth, inline.width - inline.leadingCut - inline.trailingCut);
+	
+			if (!currentLineWidth) {
+				currentLineWidth = {width: 0, leadingCut: inline.leadingCut, trailingCut: 0};
+			}
+	
+			currentLineWidth.width += inline.width;
+			currentLineWidth.trailingCut = inline.trailingCut;
+	
+			maxWidth = Math.max(maxWidth, getTrimmedWidth(currentLineWidth));
+	
+			if (inline.lineEnd) {
+				currentLineWidth = null;
+			}
+		});
+	
+		if (getStyleProperty({}, styleContextStack, 'noWrap', false)) {
+			minWidth = maxWidth;
+		}
+	
+		return {
+			items: measured,
+			minWidth: minWidth,
+			maxWidth: maxWidth
+		};
+	
+		function getTrimmedWidth(item) {
+			return Math.max(0, item.width - item.leadingCut - item.trailingCut);
+		}
+	}
+	/**
+	 * Returns size of the specified string (without breaking it) using the current style
+	 * @param  {String} text              text to be measured
+	 * @param  {Object} styleContextStack current style stack
+	 * @return {Object}                   size of the specified string
+	 */
+	sizeOfString(text, styleContextStack) {
+		text = text ? text.toString().replace(/\t/g, '    ') : '';
+	
+		//TODO: refactor - extract from measure
+		const fontName = getStyleProperty({}, styleContextStack, 'font', 'Roboto');
+		const fontSize = getStyleProperty({}, styleContextStack, 'fontSize', 12);
+		const fontFeatures = getStyleProperty({}, styleContextStack, 'fontFeatures', null);
+		const bold = getStyleProperty({}, styleContextStack, 'bold', false);
+		const italics = getStyleProperty({}, styleContextStack, 'italics', false);
+		const lineHeight = getStyleProperty({}, styleContextStack, 'lineHeight', 1);
+		const characterSpacing = getStyleProperty({}, styleContextStack, 'characterSpacing', 0);
+	
+		const font = this.fontProvider.provideFont(fontName, bold, italics);
+	
+		return {
+			width: widthOfString(text, font, fontSize, characterSpacing, fontFeatures),
+			height: font.lineHeight(fontSize) * lineHeight,
+			fontSize: fontSize,
+			lineHeight: lineHeight,
+			ascender: font.ascender / 1000 * fontSize,
+			descender: font.descender / 1000 * fontSize
+		};
+	}
+
+	widthOfString(text, font, fontSize, characterSpacing, fontFeatures) {
+		return widthOfString(text, font, fontSize, characterSpacing, fontFeatures);
+	}
 }
 
 module.exports = TextTools;
