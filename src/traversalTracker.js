93754ecd3dd17c2e3d9cd7e73aee960be175bcfe
Dorothy Thurston <dorothy.thurston@autodesk.com>
2018-11-15 19:09:53 -0800

    Update traversalTracker.js

--- src/traversalTracker.js
+++ src/traversalTracker.js
@@ -1,47 +1,49 @@
 'use strict';
 
-function TraversalTracker() {
-	this.events = {};
+class TraversalTracker {
+	constructor() {
+		this.events = {};
+	}
+
+	startTracking(event, callback) {
+		const callbacks = this.events[event] || (this.events[event] = []);
+	
+		if (callbacks.indexOf(callback) < 0) {
+			callbacks.push(callback);
+		}
+	}
+
+	stopTracking(event, callback) {
+		const callbacks = this.events[event];
+	
+		if (!callbacks) {
+			return;
+		}
+	
+		const index = callbacks.indexOf(callback);
+		if (index >= 0) {
+			callbacks.splice(index, 1);
+		}
+	}
+
+	emit(event) {
+		const args = Array.prototype.slice.call(arguments, 1);
+		const callbacks = this.events[event];
+	
+		if (!callbacks) {
+			return;
+		}
+	
+		callbacks.forEach(function (callback) {
+			callback.apply(this, args);
+		});
+	}
+
+	auto(event, callback, innerFunction) {
+		this.startTracking(event, callback);
+		innerFunction();
+		this.stopTracking(event, callback);
+	}
 }
-
-TraversalTracker.prototype.startTracking = function (event, callback) {
-	var callbacks = this.events[event] || (this.events[event] = []);
-
-	if (callbacks.indexOf(callback) < 0) {
-		callbacks.push(callback);
-	}
-};
-
-TraversalTracker.prototype.stopTracking = function (event, callback) {
-	var callbacks = this.events[event];
-
-	if (!callbacks) {
-		return;
-	}
-
-	var index = callbacks.indexOf(callback);
-	if (index >= 0) {
-		callbacks.splice(index, 1);
-	}
-};
-
-TraversalTracker.prototype.emit = function (event) {
-	var args = Array.prototype.slice.call(arguments, 1);
-	var callbacks = this.events[event];
-
-	if (!callbacks) {
-		return;
-	}
-
-	callbacks.forEach(function (callback) {
-		callback.apply(this, args);
-	});
-};
-
-TraversalTracker.prototype.auto = function (event, callback, innerFunction) {
-	this.startTracking(event, callback);
-	innerFunction();
-	this.stopTracking(event, callback);
-};
 
 module.exports = TraversalTracker;
