5a3cec5c7c58af7216b3f42754dfb711e769fa8c
Libor M <liborm85@gmail.com>
2018-12-27 14:53:53 +0100

    tests - added tests for PDFDocument.provideFont

--- tests/PDFDocument.spec.js
+++ tests/PDFDocument.spec.js
@@ -5,10 +5,17 @@ var assert = require('assert');
 var PDFDocument = require('../js/PDFDocument').default;
 
 describe('PDFDocument', function () {
-	var pdfDocument, fontDefinitions;
+	var pdfDocument;
 
 	beforeEach(function () {
-		fontDefinitions = {};
+		var fontDefinitions = {
+			Roboto: {
+				normal: 'tests/fonts/Roboto-Regular.ttf',
+				bold: 'tests/fonts/Roboto-Medium.ttf',
+				italics: 'tests/fonts/Roboto-Italic.ttf',
+				bolditalics: 'tests/fonts/Roboto-MediumItalic.ttf'
+			}
+		};
 		pdfDocument = new PDFDocument(fontDefinitions);
 	});
 
@@ -21,6 +28,26 @@ describe('PDFDocument', function () {
 				assert.equal(error.message, `Font 'Arial' in style 'bold' is not defined in the font section of the document definition.`);
 				return true;
 			});
+		});
+
+		it('should provide normal Roboto font', function () {
+			var result = pdfDocument.provideFont('Roboto', false, false);
+			assert.equal(result.font.postscriptName, 'Roboto-Regular');
+		});
+
+		it('should provide bold Roboto font', function () {
+			var result = pdfDocument.provideFont('Roboto', true, false);
+			assert.equal(result.font.postscriptName, 'Roboto-Medium');
+		});
+
+		it('should provide italics Roboto font', function () {
+			var result = pdfDocument.provideFont('Roboto', false, true);
+			assert.equal(result.font.postscriptName, 'Roboto-Italic');
+		});
+
+		it('should provide bold and italics Roboto font', function () {
+			var result = pdfDocument.provideFont('Roboto', true, true);
+			assert.equal(result.font.postscriptName, 'Roboto-MediumItalic');
 		});
 
 	});
