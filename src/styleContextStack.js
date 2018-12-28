446da403885650fbefb8b46edcaf276c021a1139
Libor M <liborm85@gmail.com>
2018-12-28 13:48:33 +0100

    refactor styleContextStack

--- src/styleContextStack.js
+++ src/styleContextStack.js
@@ -1,4 +1,4 @@
-import { isString, isArray, isUndefined, isNull } from './helpers/variableType';
+import { isString, isArray, isValue } from './helpers/variableType';
 
 /**
  * Used for style inheritance and style overrides
@@ -9,9 +9,9 @@ class StyleContextStack {
 	 * @param {Object} styleDictionary named styles dictionary
 	 * @param {Object} defaultStyle optional default style definition
 	 */
-	constructor(styleDictionary, defaultStyle) {
-		this.defaultStyle = defaultStyle || {};
+	constructor(styleDictionary, defaultStyle = {}) {
 		this.styleDictionary = styleDictionary;
+		this.defaultStyle = defaultStyle;
 		this.styleOverrides = [];
 	}
 
@@ -20,7 +20,7 @@ class StyleContextStack {
 	 * @return {StyleContextStack} current stack snapshot
 	 */
 	clone() {
-		var stack = new StyleContextStack(this.styleDictionary, this.defaultStyle);
+		let stack = new StyleContextStack(this.styleDictionary, this.defaultStyle);
 
 		this.styleOverrides.forEach(item => {
 			stack.styleOverrides.push(item);
@@ -64,7 +64,7 @@ class StyleContextStack {
 			return 0;
 		}
 
-		var styleNames = [];
+		let styleNames = [];
 
 		if (item.style) {
 			if (isArray(item.style)) {
@@ -74,11 +74,11 @@ class StyleContextStack {
 			}
 		}
 
-		for (var i = 0, l = styleNames.length; i < l; i++) {
+		for (let i = 0, l = styleNames.length; i < l; i++) {
 			this.push(styleNames[i]);
 		}
 
-		var styleProperties = [
+		let styleProperties = [
 			'font',
 			'fontSize',
 			'fontFeatures',
@@ -104,11 +104,11 @@ class StyleContextStack {
 			// 'evenRowCellBorder',
 			// 'tableBorder'
 		];
-		var styleOverrideObject = {};
-		var pushStyleOverrideObject = false;
+		let styleOverrideObject = {};
+		let pushStyleOverrideObject = false;
 
 		styleProperties.forEach(key => {
-			if (!isUndefined(item[key]) && !isNull(item[key])) {
+			if (isValue(item[key])) {
 				styleOverrideObject[key] = item[key];
 				pushStyleOverrideObject = true;
 			}
@@ -130,8 +130,8 @@ class StyleContextStack {
 	 * @return {Object} value returned by callback
 	 */
 	auto(item, callback) {
-		var pushedItems = this.autopush(item);
-		var result = callback();
+		let pushedItems = this.autopush(item);
+		let result = callback();
 
 		if (pushedItems > 0) {
 			this.pop(pushedItems);
@@ -148,17 +148,15 @@ class StyleContextStack {
 	 */
 	getProperty(property) {
 		if (this.styleOverrides) {
-			for (var i = this.styleOverrides.length - 1; i >= 0; i--) {
-				var item = this.styleOverrides[i];
+			for (let i = this.styleOverrides.length - 1; i >= 0; i--) {
+				let item = this.styleOverrides[i];
 
-				if (isString(item)) {
-					// named-style-override
-					var style = this.styleDictionary[item];
-					if (style && !isUndefined(style[property]) && !isNull(style[property])) {
+				if (isString(item)) { // named-style-override
+					let style = this.styleDictionary[item];
+					if (style && isValue(style[property])) {
 						return style[property];
 					}
-				} else if (!isUndefined(item[property]) && !isNull(item[property])) {
-					// style-overrides-object
+				} else if (isValue(item[property])) { // style-overrides-object
 					return item[property];
 				}
 			}
