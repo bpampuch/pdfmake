052cff00ec7473480821c22ca62a786a2bab1372
Libor M <liborm85@gmail.com>
2018-12-28 14:32:37 +0100

    refactor column calculator

--- src/columnCalculator.js
+++ src/columnCalculator.js
@@ -1,14 +1,14 @@
 import { isString } from './helpers/variableType';
 
 function buildColumnWidths(columns, availableWidth) {
-	var autoColumns = [];
-	var autoMin = 0;
-	var autoMax = 0;
-	var starColumns = [];
-	var starMaxMin = 0;
-	var starMaxMax = 0;
-	var fixedColumns = [];
-	var initial_availableWidth = availableWidth;
+	let autoColumns = [];
+	let autoMin = 0;
+	let autoMax = 0;
+	let starColumns = [];
+	let starMaxMin = 0;
+	let starMaxMax = 0;
+	let fixedColumns = [];
+	let initial_availableWidth = availableWidth;
 
 	columns.forEach(column => {
 		if (isAutoColumn(column)) {
@@ -41,8 +41,8 @@ function buildColumnWidths(columns, availableWidth) {
 	// http://www.freesoft.org/CIE/RFC/1942/18.htm
 	// http://www.w3.org/TR/CSS2/tables.html#width-layout
 	// http://dev.w3.org/csswg/css3-tables-algorithms/Overview.src.htm
-	var minW = autoMin + starMaxMin * starColumns.length;
-	var maxW = autoMax + starMaxMax * starColumns.length;
+	let minW = autoMin + starMaxMin * starColumns.length;
+	let maxW = autoMax + starMaxMax * starColumns.length;
 	if (minW >= availableWidth) {
 		// case 1 - there's no way to fit all columns within available width
 		// that's actually pretty bad situation with PDF as we have no horizontal scroll
@@ -64,18 +64,18 @@ function buildColumnWidths(columns, availableWidth) {
 			});
 		} else {
 			// maxW is too large, but minW fits within available width
-			var W = availableWidth - minW;
-			var D = maxW - minW;
+			let W = availableWidth - minW;
+			let D = maxW - minW;
 
 			autoColumns.forEach(col => {
-				var d = col._maxWidth - col._minWidth;
+				let d = col._maxWidth - col._minWidth;
 				col._calcWidth = col._minWidth + d * W / D;
 				availableWidth -= col._calcWidth;
 			});
 		}
 
 		if (starColumns.length > 0) {
-			var starSize = availableWidth / starColumns.length;
+			let starSize = availableWidth / starColumns.length;
 
 			starColumns.forEach(col => {
 				col._calcWidth = starSize;
@@ -94,13 +94,12 @@ function isStarColumn(column) {
 
 //TODO: refactor and reuse in measureTable
 function measureMinMax(columns) {
-	var result = { min: 0, max: 0 };
+	let result = { min: 0, max: 0 };
+	let maxStar = { min: 0, max: 0 };
+	let starCount = 0;
 
-	var maxStar = { min: 0, max: 0 };
-	var starCount = 0;
-
-	for (var i = 0, l = columns.length; i < l; i++) {
-		var c = columns[i];
+	for (let i = 0, l = columns.length; i < l; i++) {
+		let c = columns[i];
 
 		if (isStarColumn(c)) {
 			maxStar.min = Math.max(maxStar.min, c._minWidth);
