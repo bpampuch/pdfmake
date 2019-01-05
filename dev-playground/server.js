2e2a0594e2e41b2f60ff6db99395a5a8ec907c23
Libor M <liborm85@gmail.com>
2019-01-05 13:10:02 +0100

    dev-playground - changed for new pdfmake interface

--- dev-playground/server.js
+++ dev-playground/server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var pdfMakePrinter = require('../src/Printer');
+var pdfmake = require('../js/index');
 
 var app = express();
 
@@ -12,44 +12,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-function createPdfBinary(pdfDoc, callback) {
+function createPdfBinary(docDefinition, callback) {
+	var fonts = {
+		Roboto: {
+			normal: path.join(__dirname, '..', 'examples', '/fonts/Roboto-Regular.ttf'),
+			bold: path.join(__dirname, '..', 'examples', '/fonts/Roboto-Medium.ttf'),
+			italics: path.join(__dirname, '..', 'examples', '/fonts/Roboto-Italic.ttf'),
+			bolditalics: path.join(__dirname, '..', 'examples', '/fonts/Roboto-MediumItalic.ttf')
+		}
+	};
 
-  var fontDescriptors = {
-    Roboto: {
-      normal: path.join(__dirname, '..', 'examples', '/fonts/Roboto-Regular.ttf'),
-      bold: path.join(__dirname, '..', 'examples', '/fonts/Roboto-Medium.ttf'),
-      italics: path.join(__dirname, '..', 'examples', '/fonts/Roboto-Italic.ttf'),
-      bolditalics: path.join(__dirname, '..', 'examples', '/fonts/Roboto-MediumItalic.ttf')
-    }
-  };
+	pdfmake.setFonts(fonts);
 
-  var printer = new pdfMakePrinter(fontDescriptors);
-
-  var doc = printer.createPdfKitDocument(pdfDoc);
-
-  var chunks = [];
-  var result;
-
-  doc.on('data', function (chunk) {
-    chunks.push(chunk);
-  });
-  doc.on('end', function () {
-    result = Buffer.concat(chunks);
-    callback('data:application/pdf;base64,' + result.toString('base64'));
-  });
-  doc.end();
-
+	var pdf = pdfmake.createPdf(docDefinition);
+	pdf.getDataUrl(callback);
 }
 
 app.post('/pdf', function (req, res) {
-  eval(req.body.content);
+	eval(req.body.content);
 
-  createPdfBinary(dd, function(binary) {
-    res.contentType('application/pdf');
-    res.send(binary);
-  }, function(error) {
-    res.send('ERROR:' + error);
-  });
+	createPdfBinary(dd, function (binary) {
+		res.contentType('application/pdf');
+		res.send(binary);
+	}, function (error) {
+		res.send('ERROR:' + error);
+	});
 
 });
 
