31b7dcd241ba0844ee24ece1e6bd8c0fbf79b494
Libor M <liborm85@gmail.com>
2019-02-03 14:50:31 +0100

    catching errors in webpack
    (master commit: dbf923a66f8febd843e0134c39b4c3a34e343434)

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var eslint = require('gulp-eslint');
 var each = require('gulp-each');
 var fc2json = require('gulp-file-contents-to-json');
 var log = require('fancy-log');
-var PluginError = require('plugin-error');
 var exec = require('child_process').exec;
 const fs = require('fs');
 
@@ -18,9 +17,14 @@ var vfsAfter = ";";
 gulp.task('build', function (callback) {
 	webpack(require('./webpack.config.js'), function (err, stats) {
 		if (err) {
-			throw new PluginError("webpack", err);
+			callback(err);
+			return;
 		}
 		log("[webpack]", stats.toString({}));
+		if (stats.compilation.errors && stats.compilation.errors.length) {
+			callback(stats.compilation.errors);
+			return;
+		}
 		callback();
 	});
 });
@@ -28,9 +32,14 @@ gulp.task('build', function (callback) {
 gulp.task('buildWithStandardFonts', function (callback) {
 	webpack(require('./webpack-standardfonts.config.js'), function (err, stats) {
 		if (err) {
-			throw new PluginError("webpack", err);
+			callback(err);
+			return;
 		}
 		log("[webpack]", stats.toString({}));
+		if (stats.compilation.errors && stats.compilation.errors.length) {
+			callback(stats.compilation.errors);
+			return;
+		}
 		callback();
 	});
 });
