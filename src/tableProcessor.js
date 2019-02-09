ecf05d04bc7227a521364ad2500239f3c80ed3c2
Libor M <liborm85@gmail.com>
2019-02-09 08:35:19 +0100

    fix table fill color render with dontBreakRows (#1622)
    (master commit 5f0295bc1077d0314811cac96bdd322c4e22330b)

--- src/tableProcessor.js
+++ src/tableProcessor.js
@@ -341,7 +341,7 @@ TableProcessor.prototype.endRow = function (rowIndex, writer, pageBreaks) {
 					var x1f = xs[i].x + (widthLeftBorder / 2);
 					var y1f = this.dontBreakRows ? y1 : y1 - (hzLineOffset / 2);
 					var x2f = xs[i + 1].x + widthRightBorder;
-					var y2f = y2 + (this.bottomLineWidth / 2);
+					var y2f = this.dontBreakRows ? y2 + this.bottomLineWidth : y2 + (this.bottomLineWidth / 2);
 					writer.addVector({
 						type: 'rect',
 						x: x1f,
