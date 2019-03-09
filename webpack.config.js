1b3e88446bb1b88ab93b8ff221fbcdf421d0dae8
Libor M <liborm85@gmail.com>
2019-03-09 07:30:16 +0100

    fix comment

--- webpack.config.js
+++ webpack.config.js
@@ -111,8 +111,8 @@ module.exports = {
 					]})
 			},
 			/* *** */
-			
-			/* temporary bugfix for FileSaver: added hack for Web Worker and device support, see https://github.com/bpampuch/pdfmake/issues/1664 */
+
+			/* temporary bugfix for FileSaver: added hack for mobile device support, see https://github.com/bpampuch/pdfmake/issues/1664 */
 			{test: /FileSaver.min.js$/, loader: StringReplacePlugin.replace({
 					replacements: [
 						{
