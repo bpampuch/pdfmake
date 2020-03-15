26953785707582525993ab51475f520ec918a46a
Libor M <liborm85@gmail.com>
2020-03-15 13:48:26 +0100

    fix support bufferPages option

--- src/base.js
+++ src/base.js
@@ -2,11 +2,9 @@ import Printer from './Printer';
 import { pack } from './helpers/tools';
 
 class pdfmake {
-	createPdf(docDefinition) {
-		let options = {};
+	createPdf(docDefinition, options = {}) {
 		options.progressCallback = this.progressCallback;
 		options.tableLayouts = this.tableLayouts;
-		// TODO: options.bufferPages
 
 		let printer = new Printer(this.fonts);
 		let doc = printer.createPdfKitDocument(docDefinition, options);
