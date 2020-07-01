a6acf5db4f4e38106b4c6bf19e2692ebdbef953c
Libor M <liborm85@gmail.com>
2020-07-01 17:25:50 +0200

    fix formatting

--- src/URLResolver.js
+++ src/URLResolver.js
@@ -2,58 +2,58 @@ import http from 'http';
 import https from 'https';
 
 const fetchUrl = url => {
-  return new Promise((resolve, reject) => {
-    const parsedUrl = new URL(url);
-    const h = (parsedUrl.protocol === 'https:') ? https : http;
+	return new Promise((resolve, reject) => {
+		const parsedUrl = new URL(url);
+		const h = (parsedUrl.protocol === 'https:') ? https : http;
 
-    h.get(url, res => {
-      const ok = res.statusCode >= 200 && res.statusCode < 300;
-      if (!ok) {
-        reject(new TypeError(`Failed to fetch (url: "${url}")`));
-      }
+		h.get(url, res => {
+			const ok = res.statusCode >= 200 && res.statusCode < 300;
+			if (!ok) {
+				reject(new TypeError(`Failed to fetch (url: "${url}")`));
+			}
 
-      const chunks = [];
-      res.on('end', () => resolve(Buffer.concat(chunks)));
-      res.on('data', d => chunks.push(d));
-    }).on('error', reject);
-  });
+			const chunks = [];
+			res.on('end', () => resolve(Buffer.concat(chunks)));
+			res.on('data', d => chunks.push(d));
+		}).on('error', reject);
+	});
 };
 
 class URLResolver {
-  constructor(fs) {
-    this.fs = fs;
-    this.resolving = {};
-  }
+	constructor(fs) {
+		this.fs = fs;
+		this.resolving = {};
+	}
 
-  resolve(url) {
-    if (!this.resolving[url]) {
-      this.resolving[url] = new Promise((resolve, reject) => {
-        if (url.toLowerCase().indexOf('https://') === 0 || url.toLowerCase().indexOf('http://') === 0) {
-          fetchUrl(url).then(buffer => {
-            this.fs.writeFileSync(url, buffer);
-            resolve();
-          }, result => {
-            reject(result);
-          });
-        } else {
-          // cannot be resolved
-          resolve();
-        }
-      });
-    }
+	resolve(url) {
+		if (!this.resolving[url]) {
+			this.resolving[url] = new Promise((resolve, reject) => {
+				if (url.toLowerCase().indexOf('https://') === 0 || url.toLowerCase().indexOf('http://') === 0) {
+					fetchUrl(url).then(buffer => {
+						this.fs.writeFileSync(url, buffer);
+						resolve();
+					}, result => {
+						reject(result);
+					});
+				} else {
+					// cannot be resolved
+					resolve();
+				}
+			});
+		}
 
-    return this.resolving[url];
-  }
+		return this.resolving[url];
+	}
 
-  resolved() {
-    return new Promise((resolve, reject) => {
-      Promise.all(Object.values(this.resolving)).then(() => {
-        resolve();
-      }, result => {
-        reject(result);
-      });
-    });
-  }
+	resolved() {
+		return new Promise((resolve, reject) => {
+			Promise.all(Object.values(this.resolving)).then(() => {
+				resolve();
+			}, result => {
+				reject(result);
+			});
+		});
+	}
 
 }
 
--- src/virtual-fs.js
+++ src/virtual-fs.js
@@ -1,65 +1,65 @@
 const normalizeFilename = filename => {
-  if (filename.indexOf(__dirname) === 0) {
-    filename = filename.substring(__dirname.length);
-  }
+	if (filename.indexOf(__dirname) === 0) {
+		filename = filename.substring(__dirname.length);
+	}
 
-  if (filename.indexOf('/') === 0) {
-    filename = filename.substring(1);
-  }
+	if (filename.indexOf('/') === 0) {
+		filename = filename.substring(1);
+	}
 
-  return filename;
+	return filename;
 };
 
 class VirtualFileSystem {
-  constructor() {
-    this.storage = {};
-  }
+	constructor() {
+		this.storage = {};
+	}
 
   /**
    * @param {string} filename
    * @returns {boolean}
    */
-  existsSync(filename) {
-    const normalizedFilename = normalizeFilename(filename);
-    return typeof this.storage[normalizedFilename] !== 'undefined';
-  }
+	existsSync(filename) {
+		const normalizedFilename = normalizeFilename(filename);
+		return typeof this.storage[normalizedFilename] !== 'undefined';
+	}
 
   /**
    * @param {string} filename
    * @param {?string|?object} options
    * @returns {string|Buffer}
    */
-  readFileSync(filename, options) {
-    const normalizedFilename = normalizeFilename(filename);
-    const encoding = typeof options === 'object' ? options.encoding : options;
+	readFileSync(filename, options) {
+		const normalizedFilename = normalizeFilename(filename);
+		const encoding = typeof options === 'object' ? options.encoding : options;
 
-    if (!this.existsSync(normalizedFilename)) {
-      throw new Error(`File '${normalizedFilename}' not found in virtual file system`);
-    }
+		if (!this.existsSync(normalizedFilename)) {
+			throw new Error(`File '${normalizedFilename}' not found in virtual file system`);
+		}
 
-    const buffer = this.storage[normalizedFilename];
-    if (encoding) {
-      return buffer.toString(encoding);
-    }
+		const buffer = this.storage[normalizedFilename];
+		if (encoding) {
+			return buffer.toString(encoding);
+		}
 
-    return buffer;
-  }
+		return buffer;
+	}
 
   /**
    * @param {string} filename
    * @param {string|Buffer} content
    * @param {?string|?object} options
    */
-  writeFileSync(filename, content, options) {
-    const normalizedFilename = normalizeFilename(filename);
-    const encoding = typeof options === 'object' ? options.encoding : options;
+	writeFileSync(filename, content, options) {
+		const normalizedFilename = normalizeFilename(filename);
+		const encoding = typeof options === 'object' ? options.encoding : options;
 
-    if (!content && !options) {
-      throw new Error('No content');
-    }
+		if (!content && !options) {
+			throw new Error('No content');
+		}
 
-    this.storage[normalizedFilename] = encoding || typeof content === 'string' ? new Buffer(content, encoding) : content;
-  }
+		this.storage[normalizedFilename] = encoding || typeof content === 'string' ? new Buffer(content, encoding) : content;
+	}
 
 }
 
