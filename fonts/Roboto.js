fcdfd1d3b3b3fe05093f8c2dc698a8104c8a10b1
Libor M <liborm85@gmail.com>
2020-07-05 14:38:19 +0200

    font packages in nodejs

---
+++ fonts/Roboto.js
@@ -0,0 +1,8 @@
+module.exports = {
+	Roboto: {
+		normal: __dirname + '/Roboto/Roboto-Regular.ttf',
+		bold: __dirname + '/Roboto/Roboto-Medium.ttf',
+		italics: __dirname + '/Roboto/Roboto-Italic.ttf',
+		bolditalics: __dirname + '/Roboto/Roboto-MediumItalic.ttf'
+	}
+};
---
+++ standard-fonts/Symbol.js
@@ -0,0 +1,5 @@
+module.exports = {
+	Symbol: {
+		normal: 'Symbol'
+	}
+};
