9ab35f4c8a073f9c3e290be51e91330e22d08d8d
Libor M <liborm85@gmail.com>
2020-08-23 14:21:50 +0200

    fix auto page height for table #2005
    master commit: edbaf2c552917cc2cc2c18909d26013921a846b9

--- src/printer.js
+++ src/printer.js
@@ -207,6 +207,8 @@ function calculatePageHeight(pages, margins) {
 			return item.item.getHeight();
 		} else if (item.item._height) {
 			return item.item._height;
+		} else if (item.type === 'vector') {
+			return item.item.y1 > item.item.y2 ? item.item.y1 : item.item.y2;
 		} else {
 			// TODO: add support for next item types
 			return 0;
@@ -214,7 +216,7 @@ function calculatePageHeight(pages, margins) {
 	}
 
 	function getBottomPosition(item) {
-		var top = item.item.y;
+		var top = item.item.y || 0;
 		var height = getItemHeight(item);
 		return top + height;
 	}
