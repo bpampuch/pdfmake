815bda284f1fafd6c98a2a00aecf1e7e0bb2bf28
Libor M <liborm85@gmail.com>
2021-03-20 17:39:56 +0100

    remove dead code after 9654a4d5be78868778a6160eab19918ba7e0d2c8

--- src/browser-extensions/index.js
+++ src/browser-extensions/index.js
@@ -17,28 +17,11 @@ let defaultClientFonts = {
 		bolditalics: 'Roboto-MediumItalic.ttf'
 	}
 };
-
-const isBrowserSupported = () => {
-	// Ensure the browser provides the level of support needed
-	if (!Object.keys || typeof Uint16Array === 'undefined') {
-		return false;
-	}
-	return true;
-};
-
 class pdfmake extends pdfmakeBase {
 	constructor() {
 		super();
 		this.urlResolver = new URLBrowserResolver(this.virtualfs);
 		this.fonts = defaultClientFonts;
-	}
-
-	createPdf(docDefinition) {
-		if (!isBrowserSupported()) {
-			throw new Error('Your browser does not provide the level of support needed');
-		}
-
-		return super.createPdf(docDefinition);
 	}
 
 	addFontContainer(fontContainer) {
