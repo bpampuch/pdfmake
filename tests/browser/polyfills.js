0695645fa60edc6174cc44d68c4354179d9492df
Libor M <liborm85@gmail.com>
2021-11-16 15:51:08 +0100

    test for core-js polyfill Object.isExtensible bug #2362
    master commit: 66952b262d3f73e2827a34a1997e9692c0467687

---
+++ tests/browser/polyfills.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+var pdfmake = require('./../../build/pdfmake.js');
+var pdfFonts = require('./../../build/vfs_fonts.js');
+pdfmake.vfs = pdfFonts.pdfMake.vfs;
+
+describe('core-js polyfill', function () {
+	it('Object.isExtensible bug', async function () {
+		var docDefinition = {
+			content: [
+				'Empty document'
+			]
+		};
+
+		var pdf = pdfmake.createPdf(docDefinition);
+		pdf.getStream(() => {
+			// noop
+		});
+
+		assert.equal(Object.isExtensible(docDefinition), true);
+	});
+});
