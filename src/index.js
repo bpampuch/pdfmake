5e340b1fd1e909fb57a4a4a56667da2261485fef
Luke <lukestagoll16@gmail.com>
2022-12-21 17:08:25 +0800

    Update exports

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const DocMeasure = require('./docMeasure');
+const DocPreprocessor = require('./docPreprocessor');
+const FontProvider = require('./fontProvider');
+const LayoutBuilder = require('./layoutBuilder');
+const PdfKitEngine = require('./pdfKitEngine');
+const Printer = require('./printer');
+const SVGMeasure = require('./svgMeasure');
+
+module.exports = {
+  DocMeasure,
+  DocPreprocessor,
+  FontProvider,
+  LayoutBuilder,
+  PdfKitEngine,
+  Printer,
+  SVGMeasure,
+}
