185c0691f0b15f7e4409d0d3c08555599807839f
New Pheeraphat <165787650+newpheeraphatflowaccount@users.noreply.github.com>
2025-10-02 15:27:22 +0700

    (chore): docMeasure,js
    Revert "(chore): docMeasure,js"
    
    This reverts commit 8f4eb4303073a65f1287573c75bd4b12d2482f04.
    
    Reapply "(chore): docMeasure,js"
    
    This reverts commit 43e4d70bd6b1e651aa0fda5eca918db658542a33.

--- src/docMeasure.js
+++ src/docMeasure.js
@@ -49,6 +49,8 @@ DocMeasure.prototype.measureNode = function (node) {
 			return extendMargins(self.measureColumns(node));
 		} else if (node.stack) {
 			return extendMargins(self.measureVerticalContainer(node));
+		} else if (node.layers) {
+			return extendMargins(self.measureLayers(node));
 		} else if (node.ul) {
 			return extendMargins(self.measureUnorderedList(node));
 		} else if (node.ol) {
@@ -284,6 +286,22 @@ DocMeasure.prototype.measureToc = function (node) {
 
 DocMeasure.prototype.measureVerticalContainer = function (node) {
 	var items = node.stack;
+
+	node._minWidth = 0;
+	node._maxWidth = 0;
+
+	for (var i = 0, l = items.length; i < l; i++) {
+		items[i] = this.measureNode(items[i]);
+
+		node._minWidth = Math.max(node._minWidth, items[i]._minWidth);
+		node._maxWidth = Math.max(node._maxWidth, items[i]._maxWidth);
+	}
+
+	return node;
+};
+
+DocMeasure.prototype.measureLayers = function (node) {
+	var items = node.layers;
 
 	node._minWidth = 0;
 	node._maxWidth = 0;
@@ -559,36 +577,38 @@ DocMeasure.prototype.measureTable = function (node) {
 	var colSpans = [];
 	var col, row, cols, rows;
 
-	for (col = 0, cols = node.table.body[0].length; col < cols; col++) {
-		var c = node.table.widths[col];
-		c._minWidth = 0;
-		c._maxWidth = 0;
+	if(node.table.body[0]) {
+		for (col = 0, cols = node.table.body[0].length; col < cols; col++) {
+			var c = node.table.widths[col];
+			c._minWidth = 0;
+			c._maxWidth = 0;
 
-		for (row = 0, rows = node.table.body.length; row < rows; row++) {
-			var rowData = node.table.body[row];
-			var data = rowData[col];
-			if (data === undefined) {
-				console.error('Malformed table row ', rowData, 'in node ', node);
-				throw 'Malformed table row, a cell is undefined.';
-			}
-			if (data === null) { // transform to object
-				data = '';
-			}
-
-			if (!data._span) {
-				data = rowData[col] = this.styleStack.auto(data, measureCb(this, data));
-
-				if (data.colSpan && data.colSpan > 1) {
-					markSpans(rowData, col, data.colSpan);
-					colSpans.push({ col: col, span: data.colSpan, minWidth: data._minWidth, maxWidth: data._maxWidth });
-				} else {
-					c._minWidth = Math.max(c._minWidth, data._minWidth);
-					c._maxWidth = Math.max(c._maxWidth, data._maxWidth);
+			for (row = 0, rows = node.table.body.length; row < rows; row++) {
+				var rowData = node.table.body[row];
+				var data = rowData[col];
+				if (data === undefined) {
+					console.error('Malformed table row ', rowData, 'in node ', node);
+					throw 'Malformed table row, a cell is undefined.';
 				}
-			}
+				if (data === null) { // transform to object
+					data = '';
+				}
 
-			if (data.rowSpan && data.rowSpan > 1) {
-				markVSpans(node.table, row, col, data.rowSpan);
+				if (!data._span) {
+					data = rowData[col] = this.styleStack.auto(data, measureCb(this, data));
+
+					if (data.colSpan && data.colSpan > 1) {
+						markSpans(rowData, col, data.colSpan);
+						colSpans.push({ col: col, span: data.colSpan, minWidth: data._minWidth, maxWidth: data._maxWidth });
+					} else {
+						c._minWidth = Math.max(c._minWidth, data._minWidth);
+						c._maxWidth = Math.max(c._maxWidth, data._maxWidth);
+					}
+				}
+
+				if (data.rowSpan && data.rowSpan > 1) {
+					markVSpans(node.table, row, col, data.rowSpan);
+				}
 			}
 		}
 	}
