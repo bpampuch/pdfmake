ac148025d4557f0aa4c0ead3d6c72ff34ea9f576
New Pheeraphat <165787650+newpheeraphatflowaccount@users.noreply.github.com>
2025-10-03 16:54:05 +0700

    (chore): migrate traversalTracker.js file

--- src/traversalTracker.js
+++ src/traversalTracker.js
@@ -1,50 +1,47 @@
-/* jslint node: true */
 'use strict';
 
-/**
- * Creates an instance of TraversalTracker
- *
- * @constructor
- */
 function TraversalTracker() {
 	this.events = {};
 }
 
-TraversalTracker.prototype.startTracking = function (event, cb) {
-	var callbacks = (this.events[event] || (this.events[event] = []));
+TraversalTracker.prototype.startTracking = function (event, callback) {
+	var callbacks = this.events[event] || (this.events[event] = []);
 
-	if (callbacks.indexOf(cb) < 0) {
-		callbacks.push(cb);
+	if (callbacks.indexOf(callback) < 0) {
+		callbacks.push(callback);
 	}
 };
 
-TraversalTracker.prototype.stopTracking = function (event, cb) {
+TraversalTracker.prototype.stopTracking = function (event, callback) {
 	var callbacks = this.events[event];
 
-	if (callbacks) {
-		var index = callbacks.indexOf(cb);
-		if (index >= 0) {
-			callbacks.splice(index, 1);
-		}
+	if (!callbacks) {
+		return;
+	}
+
+	var index = callbacks.indexOf(callback);
+	if (index >= 0) {
+		callbacks.splice(index, 1);
 	}
 };
 
 TraversalTracker.prototype.emit = function (event) {
 	var args = Array.prototype.slice.call(arguments, 1);
-
 	var callbacks = this.events[event];
 
-	if (callbacks) {
-		callbacks.forEach(function (cb) {
-			cb.apply(this, args);
-		});
+	if (!callbacks) {
+		return;
 	}
+
+	callbacks.forEach(function (callback) {
+		callback.apply(this, args);
+	});
 };
 
-TraversalTracker.prototype.auto = function (event, cb, innerBlock) {
-	this.startTracking(event, cb);
-	innerBlock();
-	this.stopTracking(event, cb);
+TraversalTracker.prototype.auto = function (event, callback, innerFunction) {
+	this.startTracking(event, callback);
+	innerFunction();
+	this.stopTracking(event, callback);
 };
 
 module.exports = TraversalTracker;
